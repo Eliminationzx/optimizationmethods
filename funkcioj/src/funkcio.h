2e16c67a996ac0eb59ea3c38e8ce65a0a1173447
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-03-26 11:47:53 +0000

    Класс funkcio сделан абстрактным. В целевые функции добавлен метод нахождения точки минимума и ряд вспомогательных.

--- funkcioj/src/funkcio.h
+++ funkcioj/src/funkcio.h
@@ -3,15 +3,29 @@
 //
 #include <QObject>
 #include <math.h>
+#include <QVector>
 //
-//! Класс для целевой функции. 
+//! Класс для целевой функции.
 class funkcio : public QObject
 {
 Q_OBJECT
+
+private:
+  //! Возвращает значение частной производной первого порядка по x1.
+  virtual double df_dx1(const Qvector<double> X);
+  //! Возвращает значение частной производной первого порядка по x2.
+  virtual double df_dx2(const Qvector<double> X);
+  //! Возвращает длину шага для оптимизации функции одной переменной.
+  virtual double lengthOfStep(const Qvector<double> X);
+  
 public:
   //! Возвращает результат вычисления функции в точке.
-  virtual double rezulto(const double x1, const double x2) { return 1;};
-  funkcio(QObject * parent = 0) : QObject(parent){};
+  virtual double rezulto(const double x1, const double x2);
+  //! Возвращает результат вычисления функции в точке.
+  virtual double rezulto(const Qvector<double> X);
+  //! Возвращает точку минимума функции.
+  virtual Qvector<double> minPoint(const double e);
+  funkcio(QObject * parent = 0) : QObject(parent);
   
 };
 
@@ -21,12 +35,28 @@ public:
 class KvadratigantoFunkcio : public funkcio
 {
 Q_OBJECT
+
 private:
   //! Коэффициенты.
   double A, B, C, D, E, F, G;
+  //! Возвращает значение частной производной первого порядка по x1.
+  double df_dx1(const Qvector<double> X)
+  { return 2*A*(X[0] - B) + E*(X[1] - G); };
+  //! Возвращает значение частной производной первого порядка по x2.
+  double df_dx2(const Qvector<double> X)
+  { return 2*C*(X[0] - D) + E*(X[1] - F); }
+  //! Возвращает длину шага для оптимизации функции одной переменной.
+  double lengthOfStep(const Qvector<double> X){};
+  	
 public:
   //! Возвращает результат вычисления функции в точке.
-  double rezulto(const double x1, const double x2) { return A*pow((x1 - B), 2) + C*pow((x2 - D), 2) + E*(x1 - F)*(x2 - G);};
+  double rezulto(const double x1, const double x2)
+  { return A*pow((x1 - B), 2) + C*pow((x2 - D), 2) + E*(x1 - F)*(x2 - G); };
+  //! Возвращает результат вычисления функции в точке.
+  double rezulto(const Qvector<double> X)
+  { return A*pow((X[0] - B), 2) + C*pow((X[1] - D), 2) + E*(X[0] - F)*(X[1] - G); };
+  //! Возвращает точку минимума функции.
+  Qvector<double> minPoint(const double e);
   KvadratigantoFunkcio(QObject * parent = 0) : funkcio(parent){
     A = 1; B = 1; C = 1; D = 1; E = 1; F = 1; G = 1;
   };
@@ -38,17 +68,33 @@ public:
 };
 
 //! Реализация целевой "овражной" функции.
-/*!  A * pow((x2 - pow(x1, 2)), 2) + B * pow((1-x1), 2)
+/*!  A*pow((x2 - pow(x1, 2)), 2) + B*pow((1-x1), 2)
  */
 class RavinaFunkcio : public funkcio
 {
 Q_OBJECT
+
 private:
   //! Коэффициенты.
   double A, B;
+  //! Возвращает значение частной производной первого порядка по x1.
+  double df_dx1(const Qvector<double> X)
+  { return -4*A*(X[1] - pow(X[0], 2)) + 2*B*(1 - X[0]); };
+  //! Возвращает значение частной производной первого порядка по x2.
+  double df_dx2(const Qvector<double> X)
+  { return 2*A*(X[1] - pow(X[0], 2)); };
+  //! Возвращает длину шага для оптимизации функции одной переменной.
+  double lengthOfStep(const Qvector<double> X){};
+  
 public:
   //! Возвращает результат вычисления функции в точке.
-  double rezulto(const double x1, const double x2) { return A * pow((x2 - pow(x1, 2)), 2) + B * pow((1-x1), 2); };
+  double rezulto(const double x1, const double x2)
+  { return A * pow((x2 - pow(x1, 2)), 2) + B * pow((1-x1), 2); };
+  //! Возвращает результат вычисления функции в точке.
+  double rezulto(const Qvector<double> X)
+  { return A * pow((X[1] - pow(X[0], 2)), 2) + B * pow((1-X[0]), 2); };
+  //! Возвращает точку минимума функции.
+  Qvector<double> minPoint(const double e);
   RavinaFunkcio(QObject * parent = 0) : funkcio(parent){ A = 1; B = 1; };
   RavinaFunkcio(double A, double B, QObject * parent = 0) : funkcio(parent){ this->A = A; this->B = B; };
 };
