f7e668a2a59cdb023c003897aabfa0edde7e1daa
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-03-27 10:50:10 +0000

    Работаю над рисованием карты высот функции. Рисует, но ужасно медленно и вешает прогу.

--- trunk/mapoporfunkcio/src/bazomapo.cpp
+++ trunk/mapoporfunkcio/src/bazomapo.cpp
@@ -1,20 +1,33 @@
 #include "bazomapo.h"
 #include "funkcio.h"
 #include <limits>
+#include <QPainter>
+#include <QColor>
+#include <QStyleOptionGraphicsItem>
 //
-BazoMapo::BazoMapo( QGraphicsItem * parent) 
-	: QGraphicsItem(parent){}
+  using namespace std;
+
+BazoMapo::BazoMapo(funkcio * Funkcio, QGraphicsItem * parent) 
+	: QGraphicsItem(parent), F(Funkcio){}
 //
 
 QRectF BazoMapo::boundingRect() const{
-  using namespace std;
   return QRectF( numeric_limits<int>::min(), numeric_limits<int>::min(),
                  numeric_limits<int>::max(), numeric_limits<int>::max());
 }
 
 
-void BazoMapo::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
-{
-  // TODO
+void BazoMapo::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
+  QColor koloro(Qt::red);
+  qreal h, s;
+  koloro.getHsvF(&h, &s, 0);
+  QRect r(option->exposedRect.toRect());
+  for(int i = r.x(); i <= r.right(); ++i){
+    for(int j = r.y(); j <= r.bottom(); ++j){
+      koloro.setHsvF(h, s, F->rezulto(i, j));
+      painter->setPen(koloro);
+      painter->drawPoint(i, j);
+    }
+  }
 }
 
--- trunk/mapoporfunkcio/src/main.cpp
+++ trunk/mapoporfunkcio/src/main.cpp
@@ -1,10 +1,11 @@
 #include <QApplication>
 #include "mainwindowimpl.h"
+#include "mapoporfunkcioimpl.h"
 //
 int main(int argc, char ** argv)
 {
 	QApplication app( argc, argv );
-	MainWindowImpl win;
+	MapoPorFunkcioImpl win;
 	win.show(); 
 	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
 	return app.exec();
