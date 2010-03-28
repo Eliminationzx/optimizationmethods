b5887c7cd8bb03c44e3eb40d4a847fec654a06a5
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-03-28 05:25:21 +0000

    Протестил нахождение минимума. Всё норм.

--- trunk/funkcioj/src/main.cpp
+++ trunk/funkcioj/src/main.cpp
@@ -1,12 +1,16 @@
 #include <QApplication>
 #include "dialogimpl.h"
 #include "funkcio.h"
+#include <QLabel>
+#include <QString>
 //
 int main(int argc, char ** argv)
 {
 	QApplication app( argc, argv );
-	DialogImpl win;
-	win.show(); 
-	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
-	return app.exec();
+	QLabel win;
+  KvadratigantoFunkcio f( 1, 0, 1, 0, 0, 0, 0, &win);
+  win.setText(QString::number(f.rezulto(f.minPoint(0.05))));
+  win.show(); 
+  app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
+  return app.exec();
 }
