0429e7478c93e34006e6ab7844c0b1ae31eae817
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-05 16:49:23 +0000

    Криво перенес.

---
+++ trunk/setvariants/src/mainwindowimpl.h
@@ -0,0 +1,43 @@
+#ifndef MAINWINDOWIMPL_H
+#define MAINWINDOWIMPL_H
+//
+#include <QMainWindow>
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <QVector>
+#include <QVariant>
+#include "ui_mainwindow.h"
+//
+class MainWindowImpl : public QMainWindow, public Ui::MainWindow
+{
+Q_OBJECT
+
+private:
+	//! Запись данных варинта в текстовый файл
+	bool WriteVariants(const QString typeFunction, const QVector<double> data);
+	//! Запись данных варинта в текстовый файл
+	bool WriteVariants(const int typeFunction, const QVector<double> data);
+	//! Чтение данных варианта из текстового файла
+	QVector<double> ReadVariants(const QString typeFunction, const int numberVariants);
+	//! Чтение данных варианта из текстового файла
+	QVector<double> ReadVariants(const int typeFunction, const int numberVariants);
+	//! Возвращает список уже существующих вариантов
+	QVector<int> AnalysisDirVariants(const QString typeFunction);
+	//! Возвращает список уже существующих вариантов
+	QVector<int> AnalysisDirVariants(const int typeFunction);
+	//! Инициализация выпадающего списка варинтов для квадратичной функции
+	void initializationQuadComboBox();
+	//! Инициализация выпадающего списка варинтов для овражной функции
+	void initializationRavinComboBox();
+
+public:
+	MainWindowImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+
+private slots:
+	void on_comboBox_ravin_activated(int index);
+	void on_comboBox_activated(int index);
+	void on_save_button_ravin_clicked();
+	void on_save_button_clicked();
+};
+#endif
