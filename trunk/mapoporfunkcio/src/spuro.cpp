f89b17ce022661fe1ed7696ebab59fb4f4c81f31
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-06 18:22:06 +0000

    Добавил инициализацию масштаба в spuro.

--- trunk/mapoporfunkcio/src/spuro.cpp
+++ trunk/mapoporfunkcio/src/spuro.cpp
@@ -1,5 +1,5 @@
 #include "spuro.h"
 //
 spuro::spuro(int IdAlgoritmo, QColor bazaKoloro, QGraphicsItem * parent)
-  : QGraphicsItem(parent), BazaKoloro(bazaKoloro), IDAlgoritmo(IdAlgoritmo){};
+  : QGraphicsItem(parent), BazaKoloro(bazaKoloro), IDAlgoritmo(IdAlgoritmo), skalo(1) {};
 //
