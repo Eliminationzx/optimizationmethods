1e44995e57bd486007b294f844d6320f1ef97dee
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-06 12:37:06 +0000

    Описал класс spuroSinkoLauxKoordinatoj.

--- mapoporfunkcio/src/spurosinkolauxkoordinatoj.cpp
+++ mapoporfunkcio/src/spurosinkolauxkoordinatoj.cpp
@@ -1,7 +1,28 @@
 #include "spurosinkolauxkoordinatoj.h"
 //
-spuroSinkoLauxKoordinatoj::spuroSinkoLauxKoordinatoj(QColor bazaKoloro, QGraphicsItem * parent) 
-	: spuro( 0, bazaKoloro, parent){
+spuroSinkoLauxKoordinatoj::spuroSinkoLauxKoordinatoj(QColor momentaKoloro, QColor bazaKoloro, QGraphicsItem * parent) 
+	: spuro( 0, bazaKoloro, parent), MomentaKoloro(momentaKoloro){
 	// TODO
 }
 //
+
+QRectF spuroSinkoLauxKoordinatoj::boundingRect() const{
+  
+}
+
+void spuroSinkoLauxKoordinatoj::finisxiIteracio(){
+  
+}
+
+void spuroSinkoLauxKoordinatoj::reveniAlMomentoPointo(){
+  
+}
+
+void spuroSinkoLauxKoordinatoj::aldoniSercxantaPointo(QPointF){
+  
+}
+void spuroSinkoLauxKoordinatoj::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widge)
+{
+  // TODO
+}
+
--- mapoporfunkcio/src/spurosinkolauxkoordinatoj.h
+++ mapoporfunkcio/src/spurosinkolauxkoordinatoj.h
@@ -4,18 +4,50 @@
 #include "spuro.h"
 //
 //! Отображет "след" покоординатного спуска с дискретным шагом.
-/*! 
+/*! Рисует прошедьшие итерации осчновныи цветом - BazaKoloro, а текущую MomentaKoloro.
  * 
  * @author Василий Почкаенко.
  * @file spurosinkolauxkoordinatoj.h
  */
 class spuroSinkoLauxKoordinatoj : public spuro
 {
-
+private:
+  //! Коэффициент масштаба.
+  qreal skalo;
+  //! Цвет текущей итерации.
+  QColor MomentaKoloro;
+  //! Точки прошедьших итераций.
+  QPolygon Vosto;
+  //! Точки текущей итерации.
+  QPolygon MomentaPointioj;
+  //! Текущая точка.
+  /*! Вокруг неё ведётся поиск.
+  */
+  QPointF MomentaPointio;
 public:
-	spuroSinkoLauxKoordinatoj(QColor bazaKoloro, //!< Основной цвет "следа".
+  //! Возвращает масштаб.
+  qreal scale() const;
+  //! Устанавливает масштаб.
+  void setScale(qreal factor);
+  //! Реализует отрисовку элемента.
+  void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widge);
+  //! Возвращает приблизительную площадь отрисовываемую элементом.
+  QRectF boundingRect() const;
+  
+	spuroSinkoLauxKoordinatoj(QColor momentaKoloro, //!< Цвет текущей итерации.
+                            QColor bazaKoloro, //!< Основной цвет "следа".
                             QGraphicsItem * parent = 0 //!< Элемент родитель.
                            );
-	
+public slots:
+  //! Завершить итерацию.
+  /*! Делает последнюю используованную в итерации точку текущей. 
+   * Переносит точки завершаемой итерации в "хвост".
+   * Заменяет список точек текущей итерации на вновь полученную текущую точку.
+   */
+  void finisxiIteracio();
+  //! Вернуться к текущей точке.
+  void reveniAlMomentoPointo();
+  //! Добавить точку поиска.
+  void aldoniSercxantaPointo(QPointF);
 };
 #endif
