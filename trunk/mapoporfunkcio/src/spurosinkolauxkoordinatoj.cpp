c4f5ec67c3fb3dd2d703c3261c234f18d9056d61
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-06 06:33:28 +0000

    Создал класс для "следа" покоординатного спуска с дискретным шагом.

---
+++ trunk/mapoporfunkcio/src/spurosinkolauxkoordinatoj.cpp
@@ -0,0 +1,7 @@
+#include "spurosinkolauxkoordinatoj.h"
+//
+spuroSinkoLauxKoordinatoj::spuroSinkoLauxKoordinatoj(QColor bazaKoloro, QGraphicsItem * parent) 
+	: spuro( 0, bazaKoloro, parent){
+	// TODO
+}
+//
