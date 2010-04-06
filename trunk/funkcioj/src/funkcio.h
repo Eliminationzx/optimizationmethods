c023549b4a74826c20a06d007a8a63731783b341
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-06 11:45:39 +0000

    Добавил два конструктора в классы целевых функций (инициализация от векторов).

--- trunk/funkcioj/src/funkcio.h
+++ trunk/funkcioj/src/funkcio.h
@@ -54,13 +54,21 @@ public:
   //! Возвращает результат вычисления функции в точке.
   double rezulto(const QVector<double> X)
   { return A*pow((X[0] - B), 2) + C*pow((X[1] - D), 2) + E*(X[0] - F)*(X[1] - G); };
+  	
+  	//! Конструктор класса KvadratigantoFunkcio по умолчанию.
   KvadratigantoFunkcio(QObject * parent = 0) : funkcio(parent){
     A = 1; B = 1; C = 1; D = 1; E = 1; F = 1; G = 1;
   };
+  //! Конструктор класса KvadratigantoFunkcio.
   KvadratigantoFunkcio(double A, double B, double C, double D, double E, double F, double G, QObject * parent = 0)
     : funkcio(parent){
     this->A = A; this->B = B; this->C = C; this->D = D; this->E = E; this->F = F; this->G = G;
   };
+	//! Конструктор класса KvadratigantoFunkcio.
+	KvadratigantoFunkcio(QVector<double> data, QObject * parent = 0) : funkcio(parent)
+ 	{
+		this->A = data[0]; this->B = data[1]; this->C = data[2]; this->D = data[3]; this->E = data[4]; this->F = data[5]; this->G = data[6];
+ 	};
   
 };
 
@@ -88,7 +96,15 @@ public:
   //! Возвращает результат вычисления функции в точке.
   double rezulto(const QVector<double> X)
   { return A * pow((X[1] - pow(X[0], 2)), 2) + B * pow((1-X[0]), 2); };
+  	
+  	//! Конструктор класса RavinaFunkcio по умолчанию.
   RavinaFunkcio(QObject * parent = 0) : funkcio(parent){ A = 1; B = 1; };
+  //! Конструктор класса RavinaFunkcio.
   RavinaFunkcio(double A, double B, QObject * parent = 0) : funkcio(parent){ this->A = A; this->B = B; };
+	//! Конструктор класса RavinaFunkcio.
+	RavinaFunkcio(QVector<double> data, QObject * parent = 0) : funkcio(parent)
+	{
+		this->A = data[0]; this->B = data[1];
+	};
 };
 #endif
