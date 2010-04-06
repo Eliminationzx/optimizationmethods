929041453850184e4884d89c58123aba4c44f598
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-06 07:05:52 +0000

    Добавил инициализацию выпадающего списка методов.

--- branches/optimizationmethods/src/mainwindowimpl.cpp
+++ branches/optimizationmethods/src/mainwindowimpl.cpp
@@ -1,8 +1,24 @@
 #include "mainwindowimpl.h"
 //
+//! Конструктор класса.
 MainWindowImpl::MainWindowImpl( QWidget * parent, Qt::WFlags f) 
 	: QMainWindow(parent, f)
 {
 	setupUi(this);
+	
+	QVariant var;
+	var.setValue(0);
+	choiceMethods->addItem(trUtf8("Метод покоординатного спуска с дискретным шагом"), var);
+	var.setValue(1);
+	choiceMethods->addItem(trUtf8("Метод покоординатного спуска с минимизацией по направлению"), var);
+	var.setValue(2);
+	choiceMethods->addItem(trUtf8("Метод наискорейшего спуска"), var);
+	var.setValue(3);
+	choiceMethods->addItem(trUtf8("Метод Хука-Дживса"), var);
+	var.setValue(4);
+	choiceMethods->addItem(trUtf8("Метод Нелдера-Мида"), var);
+	var.setValue(5);
+	choiceMethods->addItem(trUtf8("Метод Ньютона"), var);
+	choiceMethods->setCurrentIndex(-1);
 }
 //
--- branches/optimizationmethods/src/mainwindowimpl.h
+++ branches/optimizationmethods/src/mainwindowimpl.h
@@ -2,13 +2,24 @@
 #define MAINWINDOWIMPL_H
 //
 #include <QMainWindow>
+#include <QVariant>
 #include "ui_mainwindow.h"
 //
+//! Производный класс от класса главной формы приложения optimizationmethods.
+/*! Предназначен для выбора метода оптимизации и целевой функции.
+ * @author Проскурин Александр Викторович
+ * @file mainwindowimpl.h
+ */
 class MainWindowImpl : public QMainWindow, public Ui::MainWindow
 {
 Q_OBJECT
+
+private:
+
 public:
+	//! Конструктор класса.
 	MainWindowImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+
 private slots:
 };
 #endif
