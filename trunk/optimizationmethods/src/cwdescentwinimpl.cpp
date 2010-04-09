7a5989ad0647b6817f577cb8e223013feae38ea0
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-09 06:12:37 +0000

    Сделал создание карты в CWdescentWinImpl.

--- trunk/optimizationmethods/src/cwdescentwinimpl.cpp
+++ trunk/optimizationmethods/src/cwdescentwinimpl.cpp
@@ -1,8 +1,15 @@
 #include "cwdescentwinimpl.h"
+#include "mapoporfunkcioimpl.h"
 //
 CWdescentWinImpl::CWdescentWinImpl( funkcio *f, QVector<double> *d, QWidget * parent, Qt::WFlags flags) 
 	: AlgoritmoWin(f, d, parent, flags){
 	setupUi(this);
+  
+  // Создаю карту.
+  // centralwidget->layout() - указатель на компановщик центрального виджета
+  // static_cast<QGridLayout*>(centralwidget->layout()) - обьясняю компилятору, что это именно QGridLayout
+  // добавляю вижет карты в позицию 1,1. Компановщик сам позаботится о назначении новому виджету родителя.
+  static_cast<QGridLayout*>(centralwidget->layout())->addWidget(new MapoPorFunkcioImpl(F), 1, 1);
 }
 //
 
