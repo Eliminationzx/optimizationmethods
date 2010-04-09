a768868ad20d7c6047b4acac787bc312a8d6d723
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-09 12:22:33 +0000

    Описал DemonstrataQPointF.

--- optimizationmethods/src/demonstrataqpointf.cpp
+++ optimizationmethods/src/demonstrataqpointf.cpp
@@ -1,8 +1,23 @@
 #include "demonstrataqpointf.h"
 //
 DemonstrataQPointF::DemonstrataQPointF( qreal x, qreal y ) 
-	: QPointF()
-{
-	// TODO
-}
+	: QPointF(x, y){}
+DemonstrataQPointF::DemonstrataQPointF( const QPoint & point ) 
+	: QPointF(point){}
 //
+
+
+void DemonstrataQPointF::setX(qreal x){
+  // TODO
+}
+
+void DemonstrataQPointF::setY(qreal y){
+  // TODO
+}
+
+
+QPointF & DemonstrataQPointF::operator*=(qreal factor)
+{
+  // TODO
+}
+
--- optimizationmethods/src/demonstrataqpointf.h
+++ optimizationmethods/src/demonstrataqpointf.h
@@ -5,11 +5,38 @@
 #include <QObject>
 //
 //! Расширение QPointF, дла автоматического отображения его значений в окне.
+/*! К QPointF добавлены сигалы передающие его значение. Эти сигналы вызываются во всех методах изменяющих значение точки.
+ * 
+ * @author Василий Почкаенко.
+ * @file demonstrataqpointf.h
+ */
 class DemonstrataQPointF : public QObject, public QPointF
 {
 Q_OBJECT
-public:
-	DemonstrataQPointF(qreal x, qreal y);
-	
+signals:
+  //! Предоставляет значение в виде целого числа.
+  void proviziXValoro(int valoro);
+  //! Предоставляет значение в виде десятичной дроби.
+  void proviziXValoro(double valoro);
+  //! Предоставляет значение в виде строки.
+  void proviziXValoro(QString valoro);
+  //! Предоставляет значение в виде целого числа.
+  void proviziYValoro(int valoro);
+  //! Предоставляет значение в виде десятичной дроби.
+  void proviziYValoro(double valoro);
+  //! Предоставляет значение в виде виде строки.
+  void proviziYValoro(QString valoro);
+public: 
+  QPointF & operator*=(qreal factor);
+  QPointF & operator+=(const QPointF & point);
+  QPointF & operator-=(const QPointF & point);
+  QPointF & operator/=(qreal divisor);
+  //! Устанавливает новое значение X.
+  void setX(qreal x);
+  //! Устанавливает новое значение Y.
+  void setY(qreal y);
+  DemonstrataQPointF(qreal x, qreal y);
+  DemonstrataQPointF(const QPoint & point);
+  
 };
 #endif
