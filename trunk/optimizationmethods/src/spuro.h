3a2d0312533fdc6943ec9e399e392707048cbcc5
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-09 03:26:28 +0000

    Свёл в один проект выбор задания, целевые функции и карту.

---
+++ trunk/optimizationmethods/src/spuro.h
@@ -0,0 +1,45 @@
+#ifndef SPURO_H
+#define SPURO_H
+//
+#include <QGraphicsItem>
+#include <QObject>
+//
+//! Базовый класс для отображения "следа" алгоритма.
+/*! 
+ * 
+ * @author Василий Почкаенко.
+ * @file spuro.h
+ */
+class spuro : public QObject , public QGraphicsItem
+{
+Q_OBJECT
+protected:
+  //! Коэффициент масштаба.
+  qreal skalo;
+  //! Основной цвет "следа".
+  QColor BazaKoloro;
+  //! Порядковый номер алгоритма.
+  /*! 0) метод покоординатного спуска с дискретным шагом
+   * 1) метод покоординатного спуска с минимизацией по направлению
+   * 2) метод наискорейшего спуска
+   * 3) метод Хука-Дживса
+   * 4) метод Нелдера-Мида
+   * 5) метод Ньютона
+   */
+  int IDAlgoritmo;
+public:
+  //! Возвращает масштаб.
+  qreal scale() const;
+  //! Устанавливает масштаб.
+  void setScale(qreal factor);
+  //! Возвращает название алгоритма.
+  inline int proviziIdAlgoritmo(){ return IDAlgoritmo; };
+  //! Возвращает значение основного цвета.
+  inline QColor proviziBazaKoloro(){ return BazaKoloro; };
+  
+  spuro(int IdAlgoritmo, //!< Порядковый номер алгоритма.
+        QColor bazaKoloro, //!< Основной цвет "следа".
+        QGraphicsItem * parent = 0 //!< Элемент родитель.
+       );
+};
+#endif
