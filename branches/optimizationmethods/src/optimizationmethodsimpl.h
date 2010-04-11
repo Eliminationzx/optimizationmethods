e3d7c3b585848c384951c5ac1d905a817c09b0f7
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-11 11:03:45 +0000

    branches\optimizationmethods: Пытаюсь разобраться со стеком виджетов.

---
+++ branches/optimizationmethods/src/optimizationmethodsimpl.h
@@ -0,0 +1,19 @@
+#ifndef OPTIMIZATIONMETHODSTIMPL_H
+#define OPTIMIZATIONMETHODSTIMPL_H
+//
+#include <QStackedWidget>
+#include "ui_optimizationmethods.h"
+//
+class optimizationmethodsImpl : public QStackedWidget, public Ui::Main
+{
+Q_OBJECT
+public:
+	optimizationmethodsImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+private slots:
+};
+#endif
+
+
+
+
+
