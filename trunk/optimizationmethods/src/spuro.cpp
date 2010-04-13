c75815091b3334e47c04f74bf1c991f50ccae26a
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-13 16:17:48 +0000

    Реализовал все методы spuro.

--- trunk/optimizationmethods/src/spuro.cpp
+++ trunk/optimizationmethods/src/spuro.cpp
@@ -3,3 +3,13 @@
 spuro::spuro(int IdAlgoritmo, QColor bazaKoloro, QGraphicsItem * parent)
   : QGraphicsItem(parent), BazaKoloro(bazaKoloro), IDAlgoritmo(IdAlgoritmo), skalo(1) {};
 //
+
+
+void spuro::setScale(qreal factor){
+  skalo = factor;
+}
+
+
+qreal spuro::scale() const{
+  return skalo;
+}
