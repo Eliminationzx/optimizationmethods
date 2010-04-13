d53328321eaf32641b901845b7654b138b81db53
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-13 14:42:35 +0000

    Добавил в AlgoritmoWin указатели на виджет карты и на след

--- trunk/optimizationmethods/src/algoritmowin.h
+++ trunk/optimizationmethods/src/algoritmowin.h
@@ -2,7 +2,10 @@
 #define ALGORITMOWIN_H
 //
 #include <QMainWindow>
+//
 class funkcio;
+class MapoPorFunkcioImpl;
+class spuro;
 //
 //! Базовый класс для окна прохождения алгоритма.
 class AlgoritmoWin : public QMainWindow
@@ -13,6 +16,10 @@ protected:
   funkcio * F;
   //! Данные задания.
   QVector<double> * D;
+  //! Указатель на виджет - карту высот целевой функции.
+  MapoPorFunkcioImpl * MapoWdg;
+  //! Указатель на след.
+  spuro * Sp;
 public:
 	AlgoritmoWin(funkcio *f, QVector<double> *d, QWidget * parent = 0, Qt::WindowFlags flags = 0);
 	
