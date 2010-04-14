20f6e0e58107ca603e50e4dbf7388814c1af600e
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-14 22:36:52 +0000

    Создал ветку, чтоб попробовать ппеределать алгоритм

--- mapoporfunkcio/src/spuro.cpp
+++
@@ -1,5 +0,0 @@
-#include "spuro.h"
-//
-spuro::spuro(int IdAlgoritmo, QColor bazaKoloro, QGraphicsItem * parent)
-  : QGraphicsItem(parent), BazaKoloro(bazaKoloro), IDAlgoritmo(IdAlgoritmo), skalo(1) {};
-//
