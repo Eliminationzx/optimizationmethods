743004d7a4c40554cc1bb83baa65a88145cfa088
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-14 04:30:10 +0000

    Исправил орфографические ошибки в комментариях.

--- optimizationmethods/src/spurosinkolauxkoordinatoj.h
+++ optimizationmethods/src/spurosinkolauxkoordinatoj.h
@@ -54,17 +54,17 @@ public slots:
 /*! @class spuroSinkoLauxKoordinatoj
  * Этот "след" представляет собой ломаную линию, соединяющую установленные для него точки.
  * Рисует прошедшие итерации основным цветом - BazaKoloro, а текущую MomentaKoloro.
- * 
+ *
  * Этот класс не проверяет логику прохождения алгоритма. Он лишь позволяет нарисовать "след" на сцене.
- * 
+ *
  * Сразу же после создания "следа" необходимо задать начальную точку. Для этого используется слот void difiniUnuaPointo( QPointF p ).
- * 
+ *
  * В классе создана основная точка итерации - MomentaPointio. Это точка вокруг которой ведётся поиск. Для каждой итерации эта точка должна быть обновлена.
- * 
+ *
  * Чтобы добавить точку поиска воспользуйтесь слотом void aldoniSercxantaPointo(QPointF). Если точка поиска не оказалась меньше основной точки итерации, то вызовите слот void reveniAlMomentoPointo(). Не забывайте возвращаться назад.
- * 
+ *
  * Если итерация закончена вызовите слот void finisxiIteracio(). Обратите внимание, что finisxiIteracio задаёт в качестве основной точки новой итерации последнюю точку из списка завершённой итерации. Не забывайте возвращаться назад с помощью reveniAlMomentoPointo().
- * 
+ *
  * @code
 //=========Пример==============================================================
   MapoWdg->kreiSpuro(A::CWdescent_fix, Qt::blue);
@@ -79,61 +79,61 @@ public slots:
   Sp->aldoniSercxantaPointo(QPoint(20, 10));
   //! Завершаю итерацию.
   Sp->finisxiIteracio();
-  
+
   //------Итерация 2-----------------------------------------------------------
   //! Далее буду следовать алгоритму Покоординатного спуска.
   Sp->aldoniSercxantaPointo(QPoint(20+10, 10));
   //Пусть значение в новой точке неменьше чем в базовой.
   //Вернусь назад.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(20-10, 10));  
+  Sp->aldoniSercxantaPointo(QPoint(20-10, 10));
   //Пусть значение в новой точке меньше чем в базовой.
   //Ура, завершаю итерацию.
   Sp->finisxiIteracio();
   //------Итерация 3-----------------------------------------------------------
-  Sp->aldoniSercxantaPointo(QPoint(10+5, 10));  
+  Sp->aldoniSercxantaPointo(QPoint(10+5, 10));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10-5, 10));  
+  Sp->aldoniSercxantaPointo(QPoint(10-5, 10));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10, 10+5));  
+  Sp->aldoniSercxantaPointo(QPoint(10, 10+5));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10, 10-5));  
+  Sp->aldoniSercxantaPointo(QPoint(10, 10-5));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
   //Надо уменьшить шаг и прейти к следующей итерации.
   Sp->finisxiIteracio();
   //------Итерация 4-----------------------------------------------------------
-  Sp->aldoniSercxantaPointo(QPoint(10+2.5, 10));  
+  Sp->aldoniSercxantaPointo(QPoint(10+2.5, 10));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10-2.5, 10));  
+  Sp->aldoniSercxantaPointo(QPoint(10-2.5, 10));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10, 10+2.5));  
+  Sp->aldoniSercxantaPointo(QPoint(10, 10+2.5));
   //Пусть значение в новой точке меньше чем в базовой.
   Sp->finisxiIteracio();
   //------Итерация 5-----------------------------------------------------------
-  Sp->aldoniSercxantaPointo(QPoint(10+1.25, 12.5));  
+  Sp->aldoniSercxantaPointo(QPoint(10+1.25, 12.5));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10-1.25, 12.5));  
+  Sp->aldoniSercxantaPointo(QPoint(10-1.25, 12.5));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10, 12.5+1.25));  
+  Sp->aldoniSercxantaPointo(QPoint(10, 12.5+1.25));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
-  Sp->aldoniSercxantaPointo(QPoint(10, 12.5-1.25));  
+  Sp->aldoniSercxantaPointo(QPoint(10, 12.5-1.25));
   //Пусть значение в новой точке неменьше чем в базовой.
   Sp->reveniAlMomentoPointo();
   //Надо уменьшить шаг.
-  //Пусть новый шаг меньше заданой тосности.
+  //Пусть новый шаг меньше заданной тосности.
   //!!!!!!Ура, нашли минимум!!!!!!!!!!!!!!!!
 //=============================================================================
  * @endcode
- * В примере aldoniSercxantaPointo, reveniAlMomentoPointo, finisxiIteracio вызываются на пямую. Но все они являются слотами и могут быть соединены с сигналами.
+ * В примере aldoniSercxantaPointo, reveniAlMomentoPointo, finisxiIteracio вызываются на прямую. Но все они являются слотами и могут быть соединены с сигналами.
  * @author Василий Почкаенко.
  * @file spurosinkolauxkoordinatoj.h
  */
