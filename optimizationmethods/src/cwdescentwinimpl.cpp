36cb595004a421a26edbfe64b7874439509e70ac
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-17 12:19:04 +0000

    Реализовал запись логов.

--- optimizationmethods/src/cwdescentwinimpl.cpp
+++ optimizationmethods/src/cwdescentwinimpl.cpp
@@ -36,7 +36,7 @@ CWdescentWinImpl::CWdescentWinImpl( funkcio *f, QVector<double> *d, QWidget * pa
   fsign_lb->setText(QString::number(F->rezulto(MomentaPointo)));
 //  new_fsign_lb->setText(trUtf8("Неопределено"));
   
-  
+  LogTxtBrsr->append(trUtf8("Начало поиска минимума Покоординатным спуском с  фиксированным шагом."));
   
 /*//=========Пример======================================================================================Дальше прога что-то кастует.)
   //------Итерация 1-----------------------------------------------------------
@@ -115,6 +115,7 @@ void CWdescentWinImpl::on_calculate_bt_clicked()
     new_x2_lb->setText(QString::number(NovaPointo.y()));
     new_fsign_lb->setText(QString::number(F->rezulto(NovaPointo)));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->aldoniSercxantaPointo(NovaPointo);
+    LogTxtBrsr->append(trUtf8("    Совершон шаг в + по оси Х1. Базовая точка: F(%1; %2)=%3, новая точка: F(%4; %5)=%6").arg(MomentaPointo.x()).arg(MomentaPointo.y()).arg(F->rezulto(MomentaPointo)).arg(NovaPointo.x()).arg(NovaPointo.y()).arg(F->rezulto(NovaPointo)));
     FlagEtapo = 2;
   }else if(FlagEtapo == 3 && down_x1_rb->isChecked()){
     // Нажата calculate_bt и выбрана down_x1_rb - соответствует этапу 3.
@@ -123,6 +124,7 @@ void CWdescentWinImpl::on_calculate_bt_clicked()
     new_x2_lb->setText(QString::number(NovaPointo.y()));
     new_fsign_lb->setText(QString::number(F->rezulto(NovaPointo)));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->aldoniSercxantaPointo(NovaPointo);
+    LogTxtBrsr->append(trUtf8("    Совершон шаг в - по оси Х1. Базовая точка: F(%1; %2)=%3, новая точка: F(%4; %5)=%6").arg(MomentaPointo.x()).arg(MomentaPointo.y()).arg(F->rezulto(MomentaPointo)).arg(NovaPointo.x()).arg(NovaPointo.y()).arg(F->rezulto(NovaPointo)));
     FlagEtapo = 4;
   }else if(FlagEtapo == 5 && up_x2_rb->isChecked()){
     // Нажата calculate_bt и выбрана up_x2_rb - соответствует этапу 5.
@@ -131,6 +133,7 @@ void CWdescentWinImpl::on_calculate_bt_clicked()
     new_x2_lb->setText(QString::number(NovaPointo.y()));
     new_fsign_lb->setText(QString::number(F->rezulto(NovaPointo)));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->aldoniSercxantaPointo(NovaPointo);
+    LogTxtBrsr->append(trUtf8("    Совершон шаг в + по оси Х2. Базовая точка: F(%1; %2)=%3, новая точка: F(%4; %5)=%6").arg(MomentaPointo.x()).arg(MomentaPointo.y()).arg(F->rezulto(MomentaPointo)).arg(NovaPointo.x()).arg(NovaPointo.y()).arg(F->rezulto(NovaPointo)));
     FlagEtapo = 6;
   }else if(FlagEtapo == 7 && down_x2_rb->isChecked()){
     // Нажата calculate_bt и выбрана down_x2_rb - соответствует этапу 7.
@@ -139,6 +142,7 @@ void CWdescentWinImpl::on_calculate_bt_clicked()
     new_x2_lb->setText(QString::number(NovaPointo.y()));
     new_fsign_lb->setText(QString::number(F->rezulto(NovaPointo)));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->aldoniSercxantaPointo(NovaPointo);
+    LogTxtBrsr->append(trUtf8("    Совершон шаг в - по оси Х2. Базовая точка: F(%1; %2)=%3, новая точка: F(%4; %5)=%6").arg(MomentaPointo.x()).arg(MomentaPointo.y()).arg(F->rezulto(MomentaPointo)).arg(NovaPointo.x()).arg(NovaPointo.y()).arg(F->rezulto(NovaPointo)));
     FlagEtapo = 8;
   }else{
     //Пользователь ошибся.
@@ -165,7 +169,9 @@ void CWdescentWinImpl::on_accept_bt_clicked(){
     x2_lb->setText(QString::number(MomentaPointo.y()));
     distance_lb->setText( QString::number(abs(F->rezulto(MomentaPointo) - F->rezulto(AntPnt))) );
     fsign_lb->setText(QString::number(F->rezulto(MomentaPointo)));
+    LogTxtBrsr->append(trUtf8("    Принята новая точка в качестве базовой."));
     // Сразу выполняю этап 10 - проверяю условие выхода.
+    LogTxtBrsr->append(trUtf8("    Расстояние от предыдущей базовой точки до текущей: %1, требуемая точность %2").arg(abs(F->rezulto(MomentaPointo) - F->rezulto(AntPnt))).arg(strikteco));
     if(abs(F->rezulto(MomentaPointo) - F->rezulto(AntPnt)) < strikteco){
       FlagEtapo = 11;//Перехожу к ожиданию выхода. 
     }else{
@@ -174,6 +180,7 @@ void CWdescentWinImpl::on_accept_bt_clicked(){
       new_x2_lb->setText(trUtf8("Неопределено"));
       new_fsign_lb->setText(trUtf8("Неопределено"));
       ++NumeroIteracio;
+      LogTxtBrsr->append(trUtf8("Начало новой итерации. Всего итераций %1").arg(NumeroIteracio));
       FlagEtapo = 1;//Перехожу к новой итерации.
     }
     
@@ -193,6 +200,7 @@ void CWdescentWinImpl::on_not_accept_bt_clicked(){
     new_x2_lb->setText(trUtf8("Неопределено"));
     new_fsign_lb->setText(trUtf8("Неопределено"));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->reveniAlMomentoPointo();
+    LogTxtBrsr->append(trUtf8("    Новая точка не принмается."));
     FlagEtapo = 3;
   }else if(FlagEtapo == 4 && flag){
     new_x1_lb->setText(trUtf8("Неопределено"));
@@ -205,12 +213,14 @@ void CWdescentWinImpl::on_not_accept_bt_clicked(){
     new_x2_lb->setText(trUtf8("Неопределено"));
     new_fsign_lb->setText(trUtf8("Неопределено"));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->reveniAlMomentoPointo();
+    LogTxtBrsr->append(trUtf8("    Новая точка не принмается."));
     FlagEtapo = 7;
   }else if(FlagEtapo == 8 && flag){
     new_x1_lb->setText(trUtf8("Неопределено"));
     new_x2_lb->setText(trUtf8("Неопределено"));
     new_fsign_lb->setText(trUtf8("Неопределено"));
     static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->reveniAlMomentoPointo();
+    LogTxtBrsr->append(trUtf8("    Новая точка не принмается."));
     FlagEtapo = 9;
   }else{
     //Пользователь ошибся.
@@ -223,6 +233,7 @@ void CWdescentWinImpl::on_not_accept_bt_clicked(){
 void CWdescentWinImpl::on_end_bt_clicked(){
   if(FlagEtapo == 11){
     //Нажата end_bt - соответствует этапу 11.
+    LogTxtBrsr->append(trUtf8("Найдена точкаминимума F(%1; %2) = % 3. Финиш.").arg(MomentaPointo.x()).arg(MomentaPointo.y()).arg(F->rezulto(MomentaPointo)));
   }else{
     //Пользователь ошибся.
     registriEraro();
@@ -234,10 +245,13 @@ void CWdescentWinImpl::on_end_bt_clicked(){
 void CWdescentWinImpl::on_change_step_bt_clicked(){
   if(FlagEtapo == 9){
     //Нажата change_step_bt - соответствует этапу 9.
+    QString s = trUtf8("Валичина шагов по Х1 = %1 и Х2 = %2 заменена на %3 и %4 соответственно, согласно модификатогу %5").arg(PasxoX1.x()).arg(PasxoX2.y());
     PasxoX1 *= strikteco;
     PasxoX2 *= strikteco;
+    s.arg(PasxoX1.x()).arg(PasxoX2.y()).arg(strikteco);
     x1_step_lb->setText(QString::number(PasxoX1.x()));
     x2_step_lb->setText(QString::number(PasxoX2.y()));
+    LogTxtBrsr->append(s);
     FlagEtapo = 1;
   }else{
     //Пользователь ошибся.
@@ -250,7 +264,7 @@ void CWdescentWinImpl::on_change_step_bt_clicked(){
 
 void CWdescentWinImpl::registriEraro(){
   ++KvantoEraroj;
-  LogTxtBrsr->append(trUtf8("Совершена ошибка. Общее количество ошибок: %1").arg(KvantoEraroj));
+  LogTxtBrsr->append(trUtf8("    Совершена ошибка. Общее количество ошибок: %1").arg(KvantoEraroj));
   QMessageBox msg(QMessageBox::Warning, trUtf8("Ошибка"), trUtf8("Неправильное действие"));
   msg.exec();  
 }
