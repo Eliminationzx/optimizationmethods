7dbf261912ae0cc74beb7049d872fb6641e1daf9
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-21 01:29:05 +0000

    Сделал в CWdescentWinImpl::CWdescentWinImpl вставление масштаба для карты побольше.

--- optimizationmethods/src/cwdescentwinimpl.cpp
+++ optimizationmethods/src/cwdescentwinimpl.cpp
@@ -22,7 +22,7 @@ CWdescentWinImpl::CWdescentWinImpl( funkcio *f, QVector<double> *d, QWidget * pa
   // добавляю вижет карты в позицию 1,1. Компановщик сам позаботится о назначении новому виджету родителя.
   static_cast<QGridLayout*>(centralwidget->layout())->addWidget(MapoWdg, 1, 1);
   
-  MapoWdg->setScale(3);// Ставлю масштаб побольше. Надо будет определться с оптимальным значением.
+  MapoWdg->setScale(6);// Ставлю масштаб побольше. Надо будет определться с оптимальным значением.
   
   MapoWdg->kreiSpuro(A::CWdescent_fix, Qt::blue);
   Sp = MapoWdg->proviziSpuro();
