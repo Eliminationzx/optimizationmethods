81993ef0cc86ce115a001855ca0096ca9ee547f7
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-29 17:10:47 +0000

    Описал в DemonstrataQPointF методы, предоставляющие функционал QPointF, кроме связанных не членов.

--- trunk/optimizationmethods/src/demonstrataqpointf.h
+++ trunk/optimizationmethods/src/demonstrataqpointf.h
@@ -10,15 +10,30 @@ Q_OBJECT
 private:
 	QPointF p;
 public: 
-  QPointF & operator=(const DemonstrataQPointF&);
-  QPointF & operator*=(qreal factor);
-  QPointF & operator+=(const DemonstrataQPointF & point);
-  QPointF & operator-=(const DemonstrataQPointF & point);
-  QPointF & operator/=(qreal divisor);
+  /* @return истину если обе координаты, х и у, равны 0.0, в противном случае возвращает ложь.
+  */
+	bool isNull() const;
+  /*! @return сумма абсолютных величин x() и y(),
+  */ 
+	qreal manhattanLength() const;
+  //! Возвращает значение X по ссылке.
+  qreal & rx();
+  //! Возвращает значение Y по ссылке.
+  qreal & ry();
+  //! 
+	QPoint toPoint () const;
   //! Устанавливает новое значение X.
   void setX(qreal x);
   //! Устанавливает новое значение Y.
   void setY(qreal y);
+  QPointF & operator*=(qreal factor);
+  QPointF & operator+=(const DemonstrataQPointF & point);
+  QPointF & operator-=(const DemonstrataQPointF & point);
+  QPointF & operator/=(qreal divisor);
+  //! Возвращает значение X.
+  qreal x();
+  //! Возвращает значение Y.
+  qreal y();
   DemonstrataQPointF( QObject * parent = 0);
   DemonstrataQPointF( const QPoint & point, QObject * parent = 0);
   DemonstrataQPointF( qreal x, qreal y, QObject * parent = 0);
