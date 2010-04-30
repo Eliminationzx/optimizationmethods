2c256fc74e72bdcd54620e4b46fba7f69e5a03f9
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-04-30 12:57:44 +0000

    Добавил работу с QPointF. Поправил наследование.

--- trunk/optimizationmethods/src/main.cpp
+++ trunk/optimizationmethods/src/main.cpp
@@ -1,11 +1,25 @@
 #include <QApplication>
 #include "mainwindowimpl.h"
+
+#include "demonstrataqpointf.h"
+#include "funkcio.h"
 //
 int main(int argc, char ** argv)
 {
 	QApplication app( argc, argv );
 	MainWindowImpl win;
-	win.show(); 
+	win.show();
+	
+	QPointF pf(6,7);
+	QPointF p(86,74);
+	DemonstrataQPointF dp, dp2(p);
+	dp = pf;
+	dp = dp2;
+	pf = dp;
+	KvadratigantoFunkcio f;
+	f.rezulto(pf);
+	f.rezulto(dp);
+	 	
 	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
 	return app.exec();
 }
