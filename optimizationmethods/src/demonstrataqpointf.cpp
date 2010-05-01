ad97bd8b1e2dab0d753cf9d14ee1c24a4399e755
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-01 12:17:51 +0000

    Создал класс способный высылать сигналы при изменении точки. Его экземпляр добавлен в DemonstrataQPointF. Функция SignalojPorPointF::SendiSignaloj принимает точку, чьё значение надо передать, и заставляет объект SignalojPorPointF выслать сигналы.

--- optimizationmethods/src/demonstrataqpointf.cpp
+++ optimizationmethods/src/demonstrataqpointf.cpp
@@ -14,29 +14,14 @@ DemonstrataQPointF::DemonstrataQPointF( qreal x, qreal y)
 
 void DemonstrataQPointF::setX(qreal x){
 	QPointF::setX(x);
-/*  emit proviziValoro(p);
-  emit proviziValoro(trUtf8("%1; %2").arg(QString::number(p.x())).arg(QString::number(p.y())));
-  emit proviziXValoro(p.toPoint().x());
-  emit proviziXValoro(p.x());
-  emit proviziXValoro(QString::number(p.x()));*/
 }
 
 void DemonstrataQPointF::setY(qreal y){
 	QPointF::setY(y);
-/*  emit proviziValoro(p);
-  emit proviziValoro(trUtf8("%1; %2").arg(QString::number(p.x())).arg(QString::number(p.y())));
-  emit proviziYValoro(p.toPoint().x());
-  emit proviziYValoro(p.x());
-  emit proviziYValoro(QString::number(p.x()));*/
 }
 
 QPointF & DemonstrataQPointF::operator*=(qreal factor){
 	QPointF::operator*=(factor);
-/*  emit proviziValoro(p);
-  emit proviziValoro(trUtf8("%1; %2").arg(QString::number(p.x())).arg(QString::number(p.y())));
-  emit proviziYValoro(p.toPoint().x());
-  emit proviziYValoro(p.x());
-  emit proviziYValoro(QString::number(p.x()));*/
 }
 
 QPointF & DemonstrataQPointF::operator/=(qreal divisor){
--- optimizationmethods/src/demonstrataqpointf.h
+++ optimizationmethods/src/demonstrataqpointf.h
@@ -4,13 +4,51 @@
 #include <QPointF>
 #include <QObject>
 //
-//! Обёртка QPointF, дла автоматического отображения его значений в окне.
-class DemonstrataQPointF : public QPointF{
-//Q_OBJECT
-Q_PROPERTY(qreal x READ x WRITE setX)
-Q_PROPERTY(qreal y READ y WRITE setY)
+//! Используется внутри DemonstrataQPointF. Высылет сигналы при изменении точки.
+class SignalojPorPointF: public QObject{
+Q_OBJECT
 private:
-//	QPointF p;
+public:
+	//! Высылает все сигналы.
+	void SendiSignaloj( const QPointF * p){
+		emit proviziValoro(*p);
+	  emit proviziValoro(trUtf8("%1; %2").arg(QString::number(p->x())).arg(QString::number(p->y())));
+	  emit proviziXValoro(p->x());
+	  emit proviziXValoro(p->x());
+	  emit proviziXValoro(QString::number(p->x()));
+	};
+	SignalojPorPointF( QObject * p = 0 )
+		:QObject(p){};
+signals:
+  //! Предоставляет значение в виде точки.
+  void proviziValoro(const QPointF&);
+  //! Предоставляет значение в виде строки содержащей точку.
+  void proviziValoro(const QString &);
+  //! Предоставляет значение в виде целого числа.
+  void proviziXValoro(int);
+  //! Предоставляет значение в виде десятичной дроби.
+  void proviziXValoro(double valoro);
+  //! Предоставляет значение в виде строки.
+  void proviziXValoro(const QString &);
+  //! Предоставляет значение в виде целого числа.
+  void proviziYValoro(int);
+  //! Предоставляет значение в виде десятичной дроби.
+  void proviziYValoro(double valoro);
+  //! Предоставляет значение в виде виде строки.
+  void proviziYValoro(const QString &);  
+};
+//
+/*! Расширение QPointF, для высылки сигналов с информацией об изменениях.
+ *
+ * Для QObject запрещён оператор =, поэтому обьект высылающий согналы с новыми
+ * значениями точки добавлен в DemonstrataQPointF в виде статического члена.
+ * В результате сигналы скрыты внутри DemonstrataQPointF. Чтобы соединить слот 
+ * с нужным сигналом, воспользуйтесь соответствующей функцией:
+ */
+class DemonstrataQPointF : public QPointF{
+private:
+	//! Ноаследник QObject, высылающий сигналы при изменении точки.
+	static SignalojPorPointF sp;
 public: 
   //! Устанавливает новое значение X.
   void setX(qreal x);
@@ -25,23 +63,6 @@ public:
   DemonstrataQPointF( const QPoint & point);
   DemonstrataQPointF( const QPointF & point);
   DemonstrataQPointF( qreal x, qreal y );
-//signals:
-  //! Предоставляет значение в виде точки.
-//  void proviziValoro(const QPointF&);
-  //! Предоставляет значение в виде строки содержащей точку.
-//  void proviziValoro(const QString &);
-  //! Предоставляет значение в виде целого числа.
-//  void proviziXValoro(int);
-  //! Предоставляет значение в виде десятичной дроби.
-//  void proviziXValoro(double valoro);
-  //! Предоставляет значение в виде строки.
-//  void proviziXValoro(const QString &);
-  //! Предоставляет значение в виде целого числа.
-//  void proviziYValoro(int);
-  //! Предоставляет значение в виде десятичной дроби.
-//  void proviziYValoro(double valoro);
-  //! Предоставляет значение в виде виде строки.
-//  void proviziYValoro(const QString &);  
 };
 #endif
 
