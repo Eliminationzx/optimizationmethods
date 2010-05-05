c036da682c9b87e3cfea69b514fc935a4018d1f7
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-05 16:45:41 +0000

    Поправил указатели внутри SignalojPorPointF.

--- optimizationmethods/src/demonstrataqpointf.cpp
+++ optimizationmethods/src/demonstrataqpointf.cpp
@@ -2,21 +2,25 @@
 #include <QString>
 #include <QMessageBox>
 //
-DemonstrataQPointF::DemonstrataQPointF()
-	:QPointF(), sp(new SignalojPorPointF()){
-	sp->SendiSignaloj(this);
+DemonstrataQPointF::DemonstrataQPointF(funkcio * F)
+	:QPointF(){
+	sp = new SignalojPorPointF(this, F);
+	sp->SendiSignaloj();
 }
-DemonstrataQPointF::DemonstrataQPointF( const QPoint & point)
-	:QPointF(point), sp(new SignalojPorPointF()){
-	sp->SendiSignaloj(this);
+DemonstrataQPointF::DemonstrataQPointF( const QPoint & point, funkcio * F)
+	:QPointF(point){
+	sp = new SignalojPorPointF(this, F);
+	sp->SendiSignaloj();
 }
-DemonstrataQPointF::DemonstrataQPointF( const QPointF & point )
-	:QPointF(point), sp(new SignalojPorPointF()){
-	sp->SendiSignaloj(this);
+DemonstrataQPointF::DemonstrataQPointF( const QPointF & point, funkcio * F )
+	:QPointF(point){
+	sp = new SignalojPorPointF(this, F);
+	sp->SendiSignaloj();
 }
-DemonstrataQPointF::DemonstrataQPointF( qreal x, qreal y)
-	:QPointF(x, y), sp(new SignalojPorPointF()){
-	sp->SendiSignaloj(this);
+DemonstrataQPointF::DemonstrataQPointF( qreal x, qreal y, funkcio * F)
+	:QPointF(x, y){
+	sp = new SignalojPorPointF(this, F);
+	sp->SendiSignaloj();
 }
 
 DemonstrataQPointF::~DemonstrataQPointF(){
@@ -26,47 +30,47 @@ DemonstrataQPointF::~DemonstrataQPointF(){
 
 void DemonstrataQPointF::setX(qreal x){
 	QPointF::setX(x);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 }
 
 void DemonstrataQPointF::setY(qreal y){
 	QPointF::setY(y);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 }
 
 QPointF & DemonstrataQPointF::operator*=(qreal factor){
 	QPointF::operator*=(factor);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator/=(qreal divisor){
 	QPointF::operator/=(divisor);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator+=(const QPointF & point){
 	QPointF::operator+=(point);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator-=(const QPointF & point){
 	QPointF::operator-=(point);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator=(const QPointF & point){
 	QPointF::operator=(point);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator=(const QPoint & point){
 	QPointF::operator=(point);
-	sp->SendiSignaloj(this);
+	sp->SendiSignaloj();
 	return *this;
 }
 
