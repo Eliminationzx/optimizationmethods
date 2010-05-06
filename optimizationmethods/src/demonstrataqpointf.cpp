1043ee2ce34823ce18cbfba964ffb7220d320c8a
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-06 02:38:08 +0000

    Написал комментарий к DemonstrataQPointF.

--- optimizationmethods/src/demonstrataqpointf.cpp
+++ optimizationmethods/src/demonstrataqpointf.cpp
@@ -6,46 +6,54 @@
 
 void DemonstrataQPointF::setX(qreal x){
 	QPointF::setX(x);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 }
 
 void DemonstrataQPointF::setY(qreal y){
 	QPointF::setY(y);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 }
 
 QPointF & DemonstrataQPointF::operator*=(qreal factor){
 	QPointF::operator*=(factor);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator/=(qreal divisor){
 	QPointF::operator/=(divisor);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator+=(const QPointF & point){
 	QPointF::operator+=(point);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator-=(const QPointF & point){
 	QPointF::operator-=(point);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator=(const QPointF & point){
 	QPointF::operator=(point);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 	return *this;
 }
 
 QPointF & DemonstrataQPointF::operator=(const QPoint & point){
 	QPointF::operator=(point);
+	// Высылаю сигналы, если есть объект SignalojPorPointF.
 	if(sp != 0) sp->SendiSignaloj();
 	return *this;
 }
