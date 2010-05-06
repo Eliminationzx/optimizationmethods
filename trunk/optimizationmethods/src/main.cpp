7cbf5643fb45861e592aa1c0324d74f7f5ebb780
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-06 02:10:55 +0000

    Удалил из мэйна функцию созданную для дебага.

--- trunk/optimizationmethods/src/main.cpp
+++ trunk/optimizationmethods/src/main.cpp
@@ -5,71 +5,12 @@
 #include "funkcio.h"
 #include "signalantoporpointf.h"
 //
-
-void provoDemonstrataQPointF(){
-	KvadratigantoFunkcio f ;
-	QPoint p(5,7);
-	QPointF pf(8,4);
-	DemonstrataQPointF dp;
-	DemonstrataQPointF dp1(p);
-	DemonstrataQPointF dp2(pf);
-	DemonstrataQPointF dp3(3,6);
-	
-	SignalantoPorPointF sp(&dp3, &f);
-	
-	DemonstrataQPointF dp4(dp3);
-	QPointF pf2(dp3);
-
-	dp = dp2 *= 5;
-	pf = dp2 *= 5;
-
-	dp = dp2 /= 5;
-	pf = dp2 /= 5;
-
-	dp = dp2 += dp;
-	dp = dp2 += p;
-	dp = dp2 += pf;
-	pf = dp2 += dp;
-	pf = dp2 += p;
-	pf = dp2 += pf;
-
-	dp = dp2 -= dp;
-	dp = dp2 -= p;
-	dp = dp2 -= pf;
-	pf = dp2 -= dp;
-	pf = dp2 -= p;
-	pf = dp2 -= pf;
-
-	dp = dp2 * 5;
-	pf = dp2 * 5;
-
-	dp = dp2 / 5;
-	pf = dp2 / 5;
-
-	dp = dp2 + dp;
-	dp = dp2 + p;
-	dp = dp2 + pf;
-	pf = dp2 + dp;
-	pf = dp2 + p;
-	pf = dp2 + pf;
-
-	dp = dp2 - dp;
-	dp = dp2 - p;
-	dp = dp2 - pf;
-	pf = dp2 - dp;
-	pf = dp2 - p;
-	pf = dp2 - pf;
-}
-
-//
 int main(int argc, char ** argv)
 {
-//	QApplication app( argc, argv );
-//	MainWindowImpl win;
-//	win.show();
+	QApplication app( argc, argv );
+	MainWindowImpl win;
+	win.show();
 	
-	provoDemonstrataQPointF();
-	
-//	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
-//	return app.exec();
+	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
+	return app.exec();
 }
