270c82237e0b88654d30e1058fb954a9a555e742
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-06 02:57:56 +0000

    Написал комментарий к SignalantoPorPointF. Орфография проверена.

--- trunk/optimizationmethods/src/signalantoporpointf.h
+++ trunk/optimizationmethods/src/signalantoporpointf.h
@@ -7,13 +7,43 @@ class DemonstrataQPointF;
 class QPointF;
 class funkcio;
 //
-/*! Используется внутри DemonstrataQPointF. Высылет сигналы при изменении точки.
+/*! Используется совместно c DemonstrataQPointF. Высылает сигналы при изменении 
+ * точки.
  *
- * Функция SendiSignaloj заставляет объект SignalojPorPointF выслать сигналы.
+ * Совместное использование SignalantoPorPointF и DemonstrataQPointF позволяет
+ * высылать сигналы со значениями точки, при каждом её изменении. Это позволяет
+ * соединить объект DemonstrataQPointF с элементами отображения в окне и затем
+ * работать с ним, как с обычной точкой(QPointF), не заботясь об отображении. 
+ * MOC сам всё сделает.
+ * 
+ * Пример:
+ * @code 
+ * KvadratigantoFunkcio f;
+ * DemonstrataQPointF p;
+ * SignalantoPorPointF sp(&p, &f);
+ * connect(&sp, SIGNAL(proviziValoro(const QString &)), lb, SLOT(setTEXT(const QString &)));
+ * ...
+ * p *= 6;
+ * @endcode
+ * 
+ * SignalantoPorPointF требует при создании указатель на объект 
+ * DemonstrataQPointF. Если передать 0, то ни один сигнал не будет вылан. 
+ * 
+ * Если установлен не обязательный параметр funkcio * F, то будут высылаться 
+ * сигналы со значениями преданной целевой функци в точке.
  * 
  * Сигналы SignalojPorPointF::proviziValoroFukcioEnPointo(const QString &) и
  * SignalojPorPointF::ValoroFukcioEnPointo(double) высылаются, только если 
  * установлена целевая функция.
+ * 
+ * Функция SendiSignaloj заставляет объект SignalojPorPointF выслать сигналы.
+ * Используется в DemonstrataQPointF. Можно вызвать её вручную, что бы обновить 
+ * итображения точки. Обычно вызывать вручную не требуется.
+ * 
+ * Как и любой наследник QObject, SignalantoPorPointF может быть добавлен в
+ * иерархию объектов. 
+ * 
+ * @see DemonstrataQPointF.
  * 
  * @author Василий Почкаенко.
  */
