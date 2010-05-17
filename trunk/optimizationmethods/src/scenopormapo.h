44835153e5a8928d3a65441d094005764b3140bd
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-17 08:54:29 +0000

    Почистил код ScenoPorMapo.

--- trunk/optimizationmethods/src/scenopormapo.h
+++ trunk/optimizationmethods/src/scenopormapo.h
@@ -10,46 +10,49 @@ class ScenoPorMapo : public QGraphicsScene
 {
 Q_OBJECT
 private:
-  //! Целевая функция.
-  funkcio *F;
-  //! Коэффициент масштаба.
-  qreal skalo;
+	//! Целевая функция.
+	funkcio *F;
+	//! Коэффициент масштаба.
+	qreal skalo;
 public:
-  //! Возвращает масштаб.
-  qreal scale() const;
-
-  ScenoPorMapo( funkcio * Funkcio, //!< Указатель на целевую функцию. ScenoPorMapo не заботится о назначении Funkcio родителя.
-                QObject * parent = 0 //!< Родитель.
-              );
-  ScenoPorMapo( funkcio * Funkcio, //!< Указатель на целевую функцию. ScenoPorMapo не заботится о назначении Funkcio родителя.
-                const QRectF & sceneRect, //!< Область сцены.
-                QObject * parent = 0 //!< Родитель.
-              );
-  ScenoPorMapo( funkcio * Funkcio, //!< Указатель на целевую функцию. ScenoPorMapo не заботится о назначении Funkcio родителя.
-                qreal x, //!< Горизонтальная координата левого верхнего угла сцены.
-                qreal y, //!< Вертиктальная координата левого верхнего угла сцены.
-                qreal width, //!< Ширина сцены.
-                qreal height, //!< Высота сцены.
-                QObject * parent = 0 //!< Родитель.
-              );
+	//! Возвращает масштаб.
+	qreal scale() const;
+	
+	ScenoPorMapo( funkcio * Funkcio, //!< Указатель на целевую функцию. ScenoPorMapo не заботится о назначении Funkcio родителя.
+	              QObject * parent = 0 //!< Родитель.
+	            );
+	ScenoPorMapo( funkcio * Funkcio, //!< Указатель на целевую функцию. ScenoPorMapo не заботится о назначении Funkcio родителя.
+	              const QRectF & sceneRect, //!< Область сцены.
+	              QObject * parent = 0 //!< Родитель.
+	            );
+	ScenoPorMapo( funkcio * Funkcio, //!< Указатель на целевую функцию. ScenoPorMapo не заботится о назначении Funkcio родителя.
+	              qreal x, //!< Горизонтальная координата левого верхнего угла сцены.
+	              qreal y, //!< Вертиктальная координата левого верхнего угла сцены.
+	              qreal width, //!< Ширина сцены.
+	              qreal height, //!< Высота сцены.
+	              QObject * parent = 0 //!< Родитель.
+	            );
 public slots:
-  //! Устанавливает масштаб.
-  /*! @param factor Множитель масштабирования. factor = 1 - масштаб 1:1.
-   */
-  void setScale(qreal factor);
+	//! Устанавливает масштаб.
+	/*! @param factor Множитель масштабирования. factor = 1 - масштаб 1:1.
+	 */
+	void setScale(qreal factor);
 protected:
-  //! Обработчик перемещения мыши.
-  /*! Высылаются сигналы о положении мыши. Затем вызывается реализация поумолчанию.
-   */
-  void mouseMoveEvent(QGraphicsSceneMouseEvent * mouseEvent);
-  //! Отрисовывает фон сцены с использованием painter перед отрисовкой любого элемента или переднего плана.
-  void drawBackground(QPainter * painter, //!< Контекст рисования фона сцены.
-                      const QRectF & rect //!< Область сцены.
-                     );
+	//! Обработчик перемещения мыши.
+	/*! Высылаются сигналы о положении мыши. Затем вызывается реализация поумолчанию.
+	 */
+	void mouseMoveEvent(QGraphicsSceneMouseEvent * mouseEvent);
+	//! Отрисовывает фон сцены с использованием painter перед отрисовкой любого элемента или переднего плана.
+	void drawBackground(QPainter * painter, //!< Контекст рисования фона сцены.
+	                    const QRectF & rect //!< Область сцены.
+	                   );
 signals:
-  void MusaPosX(const qreal);
-  void MusaPosY(const qreal);
-  void MusaPos(const QString );
+	//! Передаёт координату Х мыши.
+	void MusaPosX(const qreal);
+	//! Передаёт координату Y мыши.
+	void MusaPosY(const qreal);
+	//! Передаёт координаты мыши.
+	void MusaPos(const QString );
 };
 
 #endif
