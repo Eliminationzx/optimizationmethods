d2eb36c770310e554794e64304391f128ba77db8
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-23 16:11:19 +0000

    В NeMiImpl описал обработчики входа в состояния. Создал пустые тела.

--- optimizationmethods/src/nemiimpl.cpp
+++ optimizationmethods/src/nemiimpl.cpp
@@ -1,9 +1,111 @@
 #include "nemiimpl.h"
+#include "demonstrataqpointf.h"
+#include "signalantoporpointf.h"
+#include "HelpBrowser.h"
+#include "mapoporfunkcioimpl.h"
+#include "Konstantoj.h"
+#include "funkcio.h"
+#include <QTextBrowser>
+#include <QString>
+#include <QMessageBox>
+#include <QStateMachine>
+#include <QFinalState>
+#include <QLabel>
+#include <QFontDialog>
+#include <QDebug>
 //
-NeMiImpl::NeMiImpl( QWidget * parent, Qt::WFlags f) 
-	: QMainWindow(parent, f)
-{
+NeMiImpl::NeMiImpl(  funkcio *f, QVector<double> *d, QWidget * parent, Qt::WFlags flags)
+	: AlgoritmoWin(f, d, parent, flags){
 	setupUi(this);
 }
 //
 
+void NeMiImpl::init(){
+	qDebug()<<trUtf8("Вхожу в init()"); // Вывожу дебажную инфу на консоль.
+
+	precision_lb->setText(QString::number(strikteco));
+	KvantoEraroj = 0;
+	NumeroIteracio = 0;
+	P1 = QPointF(D[4],D[5]);
+	P2 = QPointF(D[4] + D[1],D[5]);
+	P3 = QPointF(D[4],D[5] + D[2]);
+	LogTxtBrsr->setText("");
+//	static_cast<spuroSinkoLauxKoordinatoj*>(Sp)->senspurigi();
+
+	qDebug()<<trUtf8("Задаю переменным начальные значения"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::so_entered(){
+
+	qDebug()<<trUtf8("Вошёл в so"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::ss_entered(){
+
+	qDebug()<<trUtf8("Вошёл в ss"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s1_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s1"); // Вывожу дебажную инфу на консоль.
+}
+
+
+void NeMiImpl::s2_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s2"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s3_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s3"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s5_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s5"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s6_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s6"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s7_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s7"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s8_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s8"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s9_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s9"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s10_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s10"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::s11_entered(){
+
+	qDebug()<<trUtf8("Вошёл в s11"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::sf_entered(){
+
+	qDebug()<<trUtf8("Вошёл в sf"); // Вывожу дебажную инфу на консоль.
+}
+
+void NeMiImpl::registriEraro(){
+  ++KvantoEraroj;
+  LogTxtBrsr->append(trUtf8("    Совершена ошибка. Общее количество ошибок: %1").arg(KvantoEraroj));
+  QMessageBox msg(QMessageBox::Warning, trUtf8("Ошибка"), trUtf8("Неправильное действие"));
+  msg.exec();
+  
+	qDebug()<<trUtf8("Пользователь ошибся"); // Вывожу дебажную инфу на консоль.
+}
--- optimizationmethods/src/nemiimpl.h
+++ optimizationmethods/src/nemiimpl.h
@@ -1,13 +1,16 @@
 #ifndef NEMIIMPL_H
 #define NEMIIMPL_H
 //
-#include <QMainWindow>
+#include "algoritmowin.h"
 #include "ui_NeMi.h"
 #include "demonstrataqpointf.h"
 #include <QSignalTransition>
 #include <QState>
 //
-class NeMiImpl : public QMainWindow, public Ui::NeMi
+class funkcio;
+//
+//! Окно для прохождения метода Нелдора-Мида.
+class NeMiImpl : public AlgoritmoWin, public Ui::NeMi
 {
 Q_OBJECT
 protected:
@@ -18,9 +21,58 @@ protected:
 	//! Третья точка основного триугольника.
 	DemonstrataQPointF P3;
 public:
-	NeMiImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+	NeMiImpl( funkcio * f, QVector<double> *d, QWidget * parent = 0,
+	          Qt::WFlags flags = 0 );
 private slots:
-	
+	/*! "Обнуление" переменных.
+	 * 
+	 * Этим методом задаются началные знаения всем переменным алгоритма.
+	 * Используется перед запуском конецного автомата и в переходе вызванным 
+	 * действием "Начать заново".
+	 */
+	void init();
+	/*! Обработчик входа в состояние so.
+	 * 
+	 * Исполизую для дебага.
+	 */
+	void so_entered();
+	/*! Обработчик входа в состояние ss.
+	 */
+	void ss_entered();
+	/*! Обработчик входа в состояние s1.
+	 */
+	void s1_entered();
+	/*! Обработчик входа в состояние s2.
+	 */
+	void s2_entered();
+	/*! Обработчик входа в состояние s3.
+	 */
+	void s3_entered();
+	/*! Обработчик входа в состояние s5.
+	 */
+	void s5_entered();
+	/*! Обработчик входа в состояние s6.
+	 */
+	void s6_entered();
+	/*! Обработчик входа в состояние s7.
+	 */
+	void s7_entered();
+	/*! Обработчик входа в состояние s8.
+	 */
+	void s8_entered();
+	/*! Обработчик входа в состояние s9.
+	 */
+	void s9_entered();
+	/*! Обработчик входа в состояние s10.
+	 */
+	void s10_entered();
+	/*! Обработчик входа в состояние s11.
+	 */
+	void s11_entered();
+	//! Обработчик входа в состояние sf - конец поиска.
+	void sf_entered();
+  //! Фиксирует совершение пользователем ошики.
+  void registriEraro();
 };
 #endif
 
