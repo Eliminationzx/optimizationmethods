21259be0890c6c3e7542203e19685fd48b2e12c0
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-25 14:23:02 +0000

    Изменил форму первого метода, чтоб карта рисовалась как можно большего размера. Пришлось поправить конструктор CWdescentWinImpl, т.к. виджет карты добавляется вручную.

--- trunk/optimizationmethods/src/cwdescentwinimpl.cpp
+++ trunk/optimizationmethods/src/cwdescentwinimpl.cpp
@@ -36,10 +36,7 @@ CWdescentWinImpl::CWdescentWinImpl( funkcio *f, QVector<double> *d, QWidget * pa
 	func->setText(formul);
 
 	// Создаю карту.
-	// centralwidget->layout() - указатель на компановщик центрального виджета
-	// static_cast<QGridLayout*>(centralwidget->layout()) - обьясняю компилятору, что это именно QGridLayout
-	// добавляю вижет карты в позицию 1,1. Компановщик сам позаботится о назначении новому виджету родителя.
-	static_cast<QGridLayout*>(centralwidget->layout())->addWidget(MapoWdg, 2, 1, 2, 1);
+	verticalLayout_4->addWidget(MapoWdg, 2);
 
 	MapoWdg->setScale(20);// Ставлю масштаб побольше. Надо будет определться с оптимальным значением.
 
