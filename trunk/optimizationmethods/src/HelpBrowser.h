3474fc6f055c43a20d34a94ef7e756ae928b9e64
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-31 07:53:25 +0000

    поправил справки.

--- trunk/optimizationmethods/src/HelpBrowser.h
+++ trunk/optimizationmethods/src/HelpBrowser.h
@@ -13,26 +13,7 @@ public:
                       QWidget* pwgt       = 0
                ) : QDialog(pwgt) 
     {
-        QPushButton*  pcmdBack    = new QPushButton("<<");
-        QPushButton*  pcmdHome    = new QPushButton("Home");
-        QPushButton*  pcmdForward = new QPushButton(">>");
         QTextBrowser* ptxtBrowser = new QTextBrowser;
-
-        connect(pcmdBack, SIGNAL(clicked()), 
-                ptxtBrowser, SLOT(backward())
-               );
-        connect(pcmdHome, SIGNAL(clicked()), 
-                ptxtBrowser, SLOT(home())
-               );
-        connect(pcmdForward, SIGNAL(clicked()), 
-                ptxtBrowser, SLOT(forward())
-               );
-        connect(ptxtBrowser, SIGNAL(backwardAvailable(bool)), 
-                pcmdBack, SLOT(setEnabled(bool))
-               );
-        connect(ptxtBrowser, SIGNAL(forwardAvailable(bool)), 
-                pcmdForward, SLOT(setEnabled(bool))
-               );
 
         ptxtBrowser->setSearchPaths(QStringList() << strPath);
         ptxtBrowser->setSource(QString(strFileName));
@@ -40,9 +21,6 @@ public:
         //Layout setup
         QVBoxLayout* pvbxLayout = new QVBoxLayout;
         QHBoxLayout* phbxLayout = new QHBoxLayout;
-        phbxLayout->addWidget(pcmdBack);
-        phbxLayout->addWidget(pcmdHome);
-        phbxLayout->addWidget(pcmdForward);
         pvbxLayout->addLayout(phbxLayout);    
         pvbxLayout->addWidget(ptxtBrowser);
         setLayout(pvbxLayout);
