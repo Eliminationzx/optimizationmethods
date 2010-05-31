3bd508ed9122d57cd4f800a2663353253eb0b764
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-05-31 05:27:25 +0000

    Полностью реализовал класс "следа" Хука-Дживса. Теперь дебажить.

--- optimizationmethods/src/spurohugi.cpp
+++ optimizationmethods/src/spurohugi.cpp
@@ -6,14 +6,24 @@
 #include <QGraphicsView>
 //
 spuroHuGi::spuroHuGi( QColor bazaKoloro, qreal Skalo, QGraphicsItem * parent )
-	: spuro( A::HuGi, bazaKoloro, Skalo, parent){
-	
-}
+	: spuro( A::HuGi, bazaKoloro, Skalo, parent), b1(0, 0){}
 //
 
 void spuroHuGi::paint(QPainter *painter, const QStyleOptionGraphicsItem */*option*/, QWidget */*widge*/){
 	painter->save();
 	
+	painter->setPen(BazaKoloro);
+	if (B2gxisP.empty());else{
+		painter->drawPolyline(aplikiScalo(B2gxisP));
+		painter->drawLine(B1gxisB2.last(), B2gxisP.first());
+		painter->drawEllipse(B2gxisP.last() * skalo, 2, 2);
+	}
+	if (B1gxisB2.empty());else{
+		painter->drawPolyline(aplikiScalo(B1gxisB2));
+		painter->drawLine(b1, B1gxisB2.first());
+		painter->drawEllipse(B1gxisB2.last() * skalo, 2, 2);
+	}
+	painter->drawEllipse(b1 * skalo, 2, 2);
 	
 	painter->restore();
 }
@@ -35,6 +45,8 @@ void spuroHuGi::difiniB2(QPointF & B2){
 }
 
 void spuroHuGi::difiniP(QPointF & P){
-	prepareGeometryChange ();
-	B2gxisP<<P;
+	if (B1gxisB2.empty());else{
+		prepareGeometryChange ();
+		B2gxisP<<P;
+	}
 }
