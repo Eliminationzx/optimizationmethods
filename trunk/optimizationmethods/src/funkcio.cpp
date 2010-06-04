924dd89f9449252768099544464ac807ce2a9a98
WillOfFear <WillOfFear@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-04 06:57:49 +0000

    исправил косяк в оваржной функции для третьего метода.

--- trunk/optimizationmethods/src/funkcio.cpp
+++ trunk/optimizationmethods/src/funkcio.cpp
@@ -42,16 +42,16 @@ double funkcio::lengthOfStep(const QPointF X) const
 	// минимум. Текущая точка (х1;х2) соответствует 0 на новой оси.
 	double a = 0;
 	// b надо взять такое, что бы в разрезе по новой оси функция имела форму чашки. 
-	double b = 0.1;
+	double b = 0.01;
 	// Изменяю b, пока не будет чашка.
-	while(rezulto(X.x() - b*df_dx1(X), X.y() - b*df_dx2(X)) < rezulto(X.x() - (b-0.09)*df_dx1(X), X.y() - (b-0.09)*df_dx2(X)))
-		b += b;
+	while(rezulto(X.x() - b*df_dx1(X), X.y() - b*df_dx2(X)) < rezulto(X.x() - (b-0.01)*df_dx1(X), X.y() - (b-0.01)*df_dx2(X)))
+		b += 0.5*b;
 	
 	double tau = 0.618033988749894;
 	double lam = a + (1 - tau)*(b - a);
 	double mu = a + tau*(b - a);
 	
-	while (fabs(b - a) > 0.0000000001)
+	while (fabs(b - a) > 0.0000001)
 	{
 		if(rezulto(X.x() - lam*df_dx1(X), X.y() - lam*df_dx2(X)) > rezulto(X.x() - mu*df_dx1(X), X.y() - mu*df_dx2(X)))
 		{
