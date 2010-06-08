fcefd47eb8775afebfbbe03018f6555f9162274c
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-08 13:32:26 +0000

    Реализовал второй след. Поправил методы, согласно изменениям. Теперь надо тестить.

--- branches/другая карта/optimizationmethods/src/spurosinkolauxkoordinatoj_md.cpp
+++ branches/другая карта/optimizationmethods/src/spurosinkolauxkoordinatoj_md.cpp
@@ -1,25 +1,23 @@
 #include "spurosinkolauxkoordinatoj_md.h"
 #include "Konstantoj.h"
 #include <qwt_plot_curve.h>
-#include <QPolygonF>
+#include <qwt_plot.h>
 //
-spuroSinkoLauxKoordinatoj_md::spuroSinkoLauxKoordinatoj_md(QColor momentaKoloro, QColor bazaKoloro) 
-	: spuro(bazaKoloro), MomentaKoloro(momentaKoloro){}
+spuroSinkoLauxKoordinatoj_md::spuroSinkoLauxKoordinatoj_md(QColor bazaKoloro, QwtPlot * Plt ) 
+	: spuro(bazaKoloro, Plt){
+	vosto = new QwtPlotCurve();
+	vosto->setPen(BazaKoloro);
+	vosto->attach(plt);
+}
 //
-
-void spuroSinkoLauxKoordinatoj_md::difiniUnuaPointo(const QPointF & p){
-}
-
-void spuroSinkoLauxKoordinatoj_md::difiniUnuaPointo( qreal x, qreal y ){
-	difiniUnuaPointo(QPointF(x, y));
-}
-
-void spuroSinkoLauxKoordinatoj_md::difiniMomentaKoloro(QColor c){
-	MomentaKoloro = c;
-}
 
 void spuroSinkoLauxKoordinatoj_md::senspurigi(){
+	vosto->setData(QPolygonF());
+	plt->replot();
 }
 
 void spuroSinkoLauxKoordinatoj_md::aldoniPointo(const QPointF & p){
+	Vosto<<p;
+	vosto->setData(Vosto);
+	plt->replot();
 }
--- branches/другая карта/optimizationmethods/src/spurosinkolauxkoordinatoj_md.h
+++ branches/другая карта/optimizationmethods/src/spurosinkolauxkoordinatoj_md.h
@@ -2,36 +2,28 @@
 #define SPUROSINKOLAUXKOORDINATOJ_MD_H
 //
 #include "spuro.h"
+#include <QPolygonF>
 //
+class QwtPlotCurve;
 //! Отображает "след" покоординатного спуска с минимизацией по направлению.
 class spuroSinkoLauxKoordinatoj_md : public spuro
 {
 Q_OBJECT
 private:
-	//! Цвет текущей итерации.
-	QColor MomentaKoloro;
+	QwtPlotCurve * vosto;
 	//! Точки прошедших итераций.
-//	QPolygonF Vosto;
+	QPolygonF Vosto;
 	//! Точки текущей итерации.
 //	QPolygonF MomentaPointoj;
 	//! Текущая точка.
 //	QPointF MomentaPointo;
 public:
-	spuroSinkoLauxKoordinatoj_md(QColor momentaKoloro, //!< Цвет текущей итерации.
-	                             QColor bazaKoloro //!< Основной цвет "следа".
+	spuroSinkoLauxKoordinatoj_md(QColor bazaKoloro, //!< Основной цвет "следа".
+	                             QwtPlot * Plt = 0 //!< Полотно, на котором будет нарисован след.
 	                            );
 public slots:
-	/*! Установить первую точку.
-	 * 
-	 * Устанавливает точку с которой начинается поиск.
-	 */
-	void difiniUnuaPointo(const QPointF & p );
-	//! Перегружает difiniUnuaPointo(QPointF &p).
-	void difiniUnuaPointo( qreal x, qreal y );
 	//! Добавить точку.
 	void aldoniPointo( const QPointF & p );
-	//! Установить текущий цвет.
-	void difiniMomentaKoloro(QColor momentaKoloro);
 	//! Очищает "след".
 	void senspurigi();
 };
