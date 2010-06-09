edeee83a60f8c333955c1f367ff5310058cede89
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-09 08:22:25 +0000

    Дозаливаю новые файлы.

---
+++ optimizationmethods/src/spectrogramdata.cpp
@@ -0,0 +1,30 @@
+#include "spectrogramdata.h"
+#include "funkcio.h"
+#include "Konstantoj.h"
+
+SpectrogramData::SpectrogramData(const funkcio * F)
+	: QwtRasterData(QwtDoubleRect(-ampleksoMapo, -ampleksoMapo, 2*ampleksoMapo, 2*ampleksoMapo)), f(F){
+	min = f->rezulto(f->minPoint(0.1)[0], f->minPoint(0.1)[1]);
+	//--Ищу точку приближенную к максимальной в области карты.---------------------
+	max = F->rezulto(ampleksoMapo, ampleksoMapo);
+	QPointF maxP = QPointF(ampleksoMapo, ampleksoMapo);
+	if (F->rezulto(ampleksoMapo, -ampleksoMapo) > max) max = F->rezulto(ampleksoMapo, -ampleksoMapo);
+	if (F->rezulto(-ampleksoMapo, ampleksoMapo) > max) max = F->rezulto(-ampleksoMapo, ampleksoMapo);
+	if (F->rezulto(-ampleksoMapo, -ampleksoMapo) > max) max = F->rezulto(-ampleksoMapo, -ampleksoMapo);
+	if (F->rezulto(0, ampleksoMapo) > max) max = F->rezulto(0, ampleksoMapo);
+	if (F->rezulto(0, -ampleksoMapo) > max) max = F->rezulto(0, -ampleksoMapo);
+	if (F->rezulto(ampleksoMapo, 0) > max) max = F->rezulto(ampleksoMapo, 0);
+	if (F->rezulto(-ampleksoMapo, 0) > max) max = F->rezulto(-ampleksoMapo, 0);
+}
+
+QwtRasterData * SpectrogramData::copy() const{
+	return new SpectrogramData(f);
+}
+
+QwtDoubleInterval SpectrogramData::range() const{
+	return QwtDoubleInterval(min, max);
+}
+
+double SpectrogramData::value(double x, double y) const{
+	return f->rezulto(x, y);
+}
