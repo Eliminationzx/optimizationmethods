46e4c9b8df58538815c490700a2d61134afbffb9
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-10 08:31:52 +0000

    Добавил обновление полотна в spuro::difiniPlt( QwtPlot * Plt ).

--- branches/другая карта/optimizationmethods/src/spuro.cpp
+++ branches/другая карта/optimizationmethods/src/spuro.cpp
@@ -1,29 +1,7 @@
 #include "spuro.h"
+#include <qwt_plot.h>
 //
-spuro::spuro(int IdAlgoritmo, QColor bazaKoloro, qreal Skalo, QGraphicsItem * parent)
-	: QGraphicsItem(parent), IDAlgoritmo(IdAlgoritmo), BazaKoloro(bazaKoloro),
-		skalo(Skalo), empty(false) {};
-spuro::spuro(QGraphicsItem * parent)
-	: QGraphicsItem(parent), IDAlgoritmo(0), BazaKoloro(QColor()),
-		skalo(1), empty(true) {};
-//
-
-
-QPolygonF spuro::aplikiScalo(QPolygonF p)
-{
-	QPolygonF rez;
-	for(int i=0; i < p.count(); ++i){
-	  rez.append(p[i] * skalo);
-	}
-	return rez;
-}
-
-
-void spuro::setScale(qreal factor){
-	skalo = factor;
-}
-
-
-qreal spuro::scale() const{
-	return skalo;
+void spuro::difiniPlt( QwtPlot * Plt ){
+	plt = Plt;
+	plt->replot();
 }
