a5e1eb8e04c6a25107b15a357952ee1a946084d9
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-11 03:27:10 +0000

    Добавил поля в "след" для Хука-Дживса

--- branches/другая карта/optimizationmethods/src/spurohugi.h
+++ branches/другая карта/optimizationmethods/src/spurohugi.h
@@ -2,16 +2,24 @@
 #define SPUROHUGI_H
 //
 #include "spuro.h"
+#include <QPolygonF>
 //
+class QwtPlotCurve;
+//! Отображает "след" метода Хука-Дживса.
 class spuroHuGi : public spuro
 {
 private:
 	//! Точка B1.
-//	QPointF b1;
+	QPointF b1;
+	QwtPlotCurve * p1;
 	//! Точки от B1 до B2.
-//	QPolygonF B1gxisB2;
+	QPolygonF B1gxisB2;
+	QwtPlotCurve * b1gxisb2;
+	QwtPlotCurve * p2;
 	//! Точки от B2 до P.
-//	QPolygonF B2gxisP;
+	QPolygonF B2gxisP;
+	QwtPlotCurve * b2gxisp;
+	QwtPlotCurve * p;
 public:
 	//! Устанавливает полотно, на котором будет нарисован след.
 	void difiniPlt( QwtPlot * Plt );
