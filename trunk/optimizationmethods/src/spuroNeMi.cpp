e22c56cebf21486f7d50cd9176edc9a32121b14b
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-18 13:54:11 +0000

    Порезал старую карту в основной ветке.

--- trunk/optimizationmethods/src/spuroNeMi.cpp
+++ trunk/optimizationmethods/src/spuroNeMi.cpp
@@ -12,121 +12,121 @@ spuroNeMi::spuroNeMi(QColor momentaKoloro, QColor bazaKoloro, funkcio *f, qreal 
  
 QRectF spuroNeMi::boundingRect() const{
 	QPolygonF p;
-	p<<P1<<P2<<P3;
+//	p<<P1<<P2<<P3;
 	return (SP.boundingRect() | p.boundingRect());
 }
 
 void spuroNeMi::finisxiIteracio(){
-	prepareGeometryChange();
-	SP.clear();
+//	prepareGeometryChange();
+//	SP.clear();
 }
 
 
 void spuroNeMi::paint(QPainter *painter, const QStyleOptionGraphicsItem */*option*/, QWidget */*widge*/){
-	painter->save();
-	
-	painter->setPen(MomentaKoloro);
-	painter->drawPolyline(aplikiScalo(SP));
-	painter->setPen(BazaKoloro);
-	QPolygonF p;
-	p<<P1<<P2<<P3;
-	painter->drawPolygon(aplikiScalo(p));
-	
-	painter->restore();
+//	painter->save();
+//	
+//	painter->setPen(MomentaKoloro);
+//	painter->drawPolyline(aplikiScalo(SP));
+//	painter->setPen(BazaKoloro);
+//	QPolygonF p;
+//	p<<P1<<P2<<P3;
+//	painter->drawPolygon(aplikiScalo(p));
+//	
+//	painter->restore();
 }
 
 
 void spuroNeMi::difiniBazaKoloro(QColor c){
-	prepareGeometryChange();
-	BazaKoloro = c;
+//	prepareGeometryChange();
+//	BazaKoloro = c;
 }
 
 
 void spuroNeMi::difiniMomentaKoloro(QColor c){
-	prepareGeometryChange();
-	MomentaKoloro = c;
+//	prepareGeometryChange();
+//	MomentaKoloro = c;
 }
 
 
 void spuroNeMi::senspurigi(){
-	prepareGeometryChange();
-	SP.clear();
+//	prepareGeometryChange();
+//	SP.clear();
 }
 
 
 void spuroNeMi::difiniP1(const QPointF & p){
-	prepareGeometryChange();
-	P1 = p;
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	prepareGeometryChange();
+//	P1 = p;
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
 }
 
 
 void spuroNeMi::difiniP2(const QPointF & p){
-	prepareGeometryChange();
-	P2 = p;
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	prepareGeometryChange();
+//	P2 = p;
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
 }
 
 
 void spuroNeMi::difiniP3(const QPointF & p){
-	prepareGeometryChange();
-	P3 = p;
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	prepareGeometryChange();
+//	P3 = p;
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
 }
 
 
 void spuroNeMi::difiniPRespegulo(const QPointF & p){
-	prepareGeometryChange();
-	// Создаю полигон Хl, p, Хm.
-	// Хl - наименьшая в триуголнике, Хm - средняя.
-	if(Length(p - P1) < Length(p - P2)){
-		if(Length(p - P1) < Length(p - P3)){
-			SP<<(P1);
-			SP<<(p);
-			if(Length(p - P2) < Length(p - P3)){
-				SP<<(P2);
-			}else{
-				SP<<(P3);
-			}
-		}else{
-			SP<<(P3);
-			SP<<(p);
-			SP<<(P1);
-		}
-	}else{
-		if(Length(p - P2) < Length(p - P3)){
-			SP<<(P2);
-			SP<<(p);
-			if(Length(p - P1) < Length(p - P3)){
-				SP<<(P1);
-			}else{
-				SP<<(P3);
-			}
-		}else{
-			SP<<(P3);
-			SP<<(p);
-			SP<<(P2);
-		}
-	}
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	prepareGeometryChange();
+//	// Создаю полигон Хl, p, Хm.
+//	// Хl - наименьшая в триуголнике, Хm - средняя.
+//	if(Length(p - P1) < Length(p - P2)){
+//		if(Length(p - P1) < Length(p - P3)){
+//			SP<<(P1);
+//			SP<<(p);
+//			if(Length(p - P2) < Length(p - P3)){
+//				SP<<(P2);
+//			}else{
+//				SP<<(P3);
+//			}
+//		}else{
+//			SP<<(P3);
+//			SP<<(p);
+//			SP<<(P1);
+//		}
+//	}else{
+//		if(Length(p - P2) < Length(p - P3)){
+//			SP<<(P2);
+//			SP<<(p);
+//			if(Length(p - P1) < Length(p - P3)){
+//				SP<<(P1);
+//			}else{
+//				SP<<(P3);
+//			}
+//		}else{
+//			SP<<(P3);
+//			SP<<(p);
+//			SP<<(P2);
+//		}
+//	}
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
 }
 
 
 void spuroNeMi::difiniPDilato(const QPointF & p){
-	prepareGeometryChange();
-	SP<<p<<SP.first();
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	prepareGeometryChange();
+//	SP<<p<<SP.first();
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
 }
 
 void spuroNeMi::difiniPKompakto(const QPointF & p){
-	prepareGeometryChange();
-	SP.clear();
-	difiniPRespegulo(p);
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	prepareGeometryChange();
+//	SP.clear();
+//	difiniPRespegulo(p);
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
 }
--- trunk/optimizationmethods/src/spurosinkolauxkoordinatoj_md.cpp
+++ trunk/optimizationmethods/src/spurosinkolauxkoordinatoj_md.cpp
@@ -15,66 +15,66 @@ QRectF spuroSinkoLauxKoordinatoj_md::boundingRect() const{
 	// Размер элемента - обьединённый размер "хвоста" и текущей итерации * на
 	// масштаб + поправка, чтоб не остался старый "указатель"
 	QRectF rez = MomentaPointoj.boundingRect() | Vosto.boundingRect();
-	rez.setTopLeft(rez.topLeft() * skalo - QPointF(2, 2));
-	rez.setBottomRight(rez.bottomRight() * skalo + QPointF(2, 2));
+//	rez.setTopLeft(rez.topLeft() * skalo - QPointF(2, 2));
+//	rez.setBottomRight(rez.bottomRight() * skalo + QPointF(2, 2));
 	return rez;
 }
 
 void spuroSinkoLauxKoordinatoj_md::finisxiIteracio(){
-	Vosto += MomentaPointoj;
-	MomentaPointoj.clear();
-	MomentaPointoj.append(MomentaPointo);
-	update();// Планирую перерисовку.
+//	Vosto += MomentaPointoj;
+//	MomentaPointoj.clear();
+//	MomentaPointoj.append(MomentaPointo);
+//	update();// Планирую перерисовку.
 }
 
 
 void spuroSinkoLauxKoordinatoj_md::paint(QPainter *painter, const QStyleOptionGraphicsItem */*option*/, QWidget */*widge*/){
-	painter->save();
+//	painter->save();
 	
-	painter->setPen(BazaKoloro);
-	painter->drawPolyline(aplikiScalo(Vosto));
-	painter->setPen(MomentaKoloro);
-	painter->drawPolyline(aplikiScalo(MomentaPointoj));
-	painter->setPen(BazaKoloro);
-	painter->drawEllipse(MomentaPointoj.last() * skalo, 2, 2);
+//	painter->setPen(BazaKoloro);
+//	painter->drawPolyline(aplikiScalo(Vosto));
+//	painter->setPen(MomentaKoloro);
+//	painter->drawPolyline(aplikiScalo(MomentaPointoj));
+//	painter->setPen(BazaKoloro);
+//	painter->drawEllipse(MomentaPointoj.last() * skalo, 2, 2);
 	
-	painter->restore();
+//	painter->restore();
 }
 
 void spuroSinkoLauxKoordinatoj_md::difiniUnuaPointo(const QPointF & p){
-	MomentaPointo = p;
-	MomentaPointoj.append(p);
-	// Центрирую карту на последней точке.
-	this->scene()->views()[0]->centerOn(p * skalo);
-	update();// Планирую перерисовку.
+//	MomentaPointo = p;
+//	MomentaPointoj.append(p);
+//	// Центрирую карту на последней точке.
+//	this->scene()->views()[0]->centerOn(p * skalo);
+//	update();// Планирую перерисовку.
 }
 
 void spuroSinkoLauxKoordinatoj_md::difiniUnuaPointo( qreal x, qreal y ){
-	difiniUnuaPointo(QPointF(x, y));
+//	difiniUnuaPointo(QPointF(x, y));
 }
 
 
 void spuroSinkoLauxKoordinatoj_md::difiniBazaKoloro(QColor c){
-	BazaKoloro = c;
+//	BazaKoloro = c;
 }
 
 
 void spuroSinkoLauxKoordinatoj_md::difiniMomentaKoloro(QColor c){
-	MomentaKoloro = c;
+//	MomentaKoloro = c;
 }
 
 
 void spuroSinkoLauxKoordinatoj_md::senspurigi(){
-	prepareGeometryChange ();
-	MomentaPointoj.clear();
-	Vosto.clear();
+//	prepareGeometryChange ();
+//	MomentaPointoj.clear();
+//	Vosto.clear();
 }
 
 
 void spuroSinkoLauxKoordinatoj_md::aldoniPointo(const QPointF & p){
-	MomentaPointo = p;
-	MomentaPointoj.append(p);
-	// Прокручиваю карту, чтобы не скрывать последнюю точку.
-	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
-	update();
+//	MomentaPointo = p;
+//	MomentaPointoj.append(p);
+//	// Прокручиваю карту, чтобы не скрывать последнюю точку.
+//	this->scene()->views()[0]->ensureVisible(p.x() * skalo, p.y() * skalo, 1, 1);
+//	update();
 }
