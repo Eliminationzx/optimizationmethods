cec998e34a7a985f283f4f7565892e7c87625061
kajlbtep <kajlbtep@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-20 08:32:00 +0000

    три реализации входов ХД

--- optimizationmethods/src/hugiimpl.cpp
+++ optimizationmethods/src/hugiimpl.cpp
@@ -1,9 +1,139 @@
 #include "hugiimpl.h"
+#include "mapoporfunkcioimpl.h"
+#include "Konstantoj.h"
+#include "funkcio.h"
+#include "spuro.h"
+#include "spurosinkolauxkoordinatoj.h"
+#include "demonstrataqpointf.h"
+#include "signalantoporpointf.h"
+#include "helpbrowserimpl.h"
+#include "math.h"
+#include "aboutprogramimpl.h"
+#include <QTextBrowser>
+#include <QString>
+#include <QMessageBox>
+#include <QStateMachine>
+#include <QFinalState>
+#include <QLabel>
+#include <QFontDialog>
+#include <QDebug>
 #include <QRadioButton>
 //
 HuGiImpl::HuGiImpl( funkcio *f, QVector<double> d, QWidget * parent, Qt::WFlags flags ) 
 	: AlgoritmoWin(f, d, parent, flags){
 	setupUi(this);
+
 }
+
+void HuGiImpl::registriEraro()
+{
+  ++KvantoEraroj;
+  LogTxtBrsr->append(trUtf8("    Совершена ошибка. Общее количество ошибок: %1").arg(KvantoEraroj));
+  QMessageBox msg(QMessageBox::Warning, trUtf8("Ошибка"), trUtf8("Неправильное действие"));
+  msg.exec();
+
+	qDebug()<<trUtf8("User has wrong"); // Вывожу дебажную инфу на консоль.
+}
+
+void HuGiImpl::sf_entered()
+{
+	LogTxtBrsr->append(trUtf8("Конец алгоритма. Найден минимум функции: %1 Количество ошибок: <b>%2</b>.").arg(F->rezulto(MP)).arg(KvantoEraroj));
+	if(F->metaObject()->className() == QString("RavinaFunkcio"))
+	{
+		if(D[7] == -1)
+			LogTxtBrsr->append(trUtf8("Квадратичная функция не пройдена."));
+		else
+			LogTxtBrsr->append(trUtf8("Количество ошибок в квадратичной функции: <b>%1</b>.").arg(D[7]));
+	}
+	
+	QString str = trUtf8("Найден минимум. ");
+
+	qDebug()<<trUtf8("Конец алгоритма. Найден минимум"); // Вывожу дебажную инфу на консоль.
+
+	if(KvantoEraroj > D[6]){
+		// Слишком много ошибок.
+		str += trUtf8("Ваше количество ошибок (<b>%1</b>) превысило допустимый предел (%2). Начните заново.").arg(KvantoEraroj).arg(D[6]);
+		QMessageBox::information(this, trUtf8("Внимание"), str);
+		if(F->metaObject()->className() == QString("RavinaFunkcio"))
+			close();
+		else recomenci_acn->trigger();
+	}else{
+		str += trUtf8("Вы прошли тест. ");
+		if(F->metaObject()->className() == QString("KvadratigantoFunkcio"))
+		{
+			str += trUtf8("Сообщите преподавателю и перейдите к овражной функции. Количество ошибок: <b>%1</b>.").arg(KvantoEraroj);
+			emit usiloPlenumis(A::HuGiImpl, KvantoEraroj);
+			QMessageBox::information(this, trUtf8("Поздравляем"), str);
+			close();
+		}
+		else if(F->metaObject()->className() == QString("RavinaFunkcio"))
+		{
+			str += trUtf8("Позовите преподавателя. Количество ошибок: <b>%1</b>. ").arg(KvantoEraroj);
+		
+			if(D[7] == -1)
+				str += trUtf8("Количество ошибок в квадратичной функции: не пройдена.");
+			else
+				str += trUtf8("Количество ошибок в квадратичной функции: <b>%1</b>.").arg(D[7]);
+
+			QMessageBox::information(this, trUtf8("Поздравляем"), str);
+		}
+	}
+}
+
+void CWdescentWinImpl::s1_entered()
+{
+	FLG=false;
+	B1=B2;
+	LogTxtBrsr->append(trUtf8("Итерация № %1.").arg(++NumeroIteracio));
+	qDebug()<<trUtf8("come in s1"); // Вывожу дебажную инфу на консоль.
+	emit stateHasEntered();
+}
+
+
+void CWdescentWinImpl::s2_entered()
+{
+	qDebug()<<trUtf8("come in s2"); // Вывожу дебажную инфу на консоль.
+	emit stateHasEntered();
+}
+
+void CWdescentWinImpl::s3_entered()
+{
+	IP=&B2;
+	qDebug()<<trUtf8("come in s1"); // Вывожу дебажную инфу на консоль.
+	emit stateHasEntered();
+}
+
+
+namespace HuGi
+{
+bool KonsideriPointoTransiro::eventTest(QEvent *e){
+		// Реализация по умолчанию проверяет, что сигнал пришёл от связанной кнопки.
+		if(QSignalTransition::eventTest(e)){
+			qDebug()<<trUtf8("  Проверяю f(np) < f(mp)");
+			// Проверяю своё условие.
+			return f->rezulto(*np) < f->rezulto(*mp);
+		}else{
+			return false;
+		}
+	}
+
+	bool NoKonsideriPointoTransiro::eventTest(QEvent *e){
+		// Реализация по умолчанию проверяет, что сигнал пришёл от связанной кнопки.
+		if(QSignalTransition::eventTest(e)){
+			qDebug()<<trUtf8("  Проверяю f(np) >= f(mp)");
+			// Проверяю своё условие.
+			return f->rezulto(*np) >= f->rezulto(*mp);
+		}else{
+			return false;
+		}
+	}	
+
+
+
+}
+
+
+
+
 //
 
