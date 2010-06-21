4a8486d704101dadbbc602d49a2de36626da1126
PochkaenkoV <PochkaenkoV@ad29d4c0-fddc-f482-0fed-8c489ad043dc>
2010-06-21 04:55:43 +0000

    Поправил прикручивание карты в Хуке-Дживсе.

--- optimizationmethods/src/hugiimpl.cpp
+++ optimizationmethods/src/hugiimpl.cpp
@@ -75,9 +75,9 @@ HuGiImpl::HuGiImpl( funkcio *f, QVector<double> d, QWidget * parent, Qt::WFlags 
 //=============================================================================
 
 //===Прикручиваю карту=========================================================
-	connect(sB1, SIGNAL(proviziValoro(const QPointF &)), Sp, SLOT(difiniB1));
-	connect(sB2, SIGNAL(proviziValoro(const QPointF &)), Sp, SLOT(difiniB1));
-	connect(sP, SIGNAL(proviziValoro(const QPointF &)), Sp, SLOT(difiniB1));
+	connect(sB1, SIGNAL(proviziValoro(const QPointF &)), Sp, SLOT(difiniB1(const QPointF &)));
+	connect(sB2, SIGNAL(proviziValoro(const QPointF &)), Sp, SLOT(difiniB1(const QPointF &)));
+	connect(sP, SIGNAL(proviziValoro(const QPointF &)), Sp, SLOT(difiniB1(const QPointF &)));
 //=============================================================================
 
 //===Создаю конечный автомат.==================================================
